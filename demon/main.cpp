d842d8fc757dcc006f2234b503835f93aeeb7538
geniusjoe <843520313@qq.com>
2019-06-27 11:09:47 +0800

    Update main.cpp

--- demon/main.cpp
+++ demon/main.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 #define MAXM    500000+5
 
-const int MAXN = 500005;
+const int MAXN = 500025;
 const int M = MAXN * 40;
-
 
 /**< 树链剖分 */
 struct Edge {
@@ -27,7 +26,7 @@ int pos;
 int n, q, m, tot;
 int a[MAXN], t[MAXN];
 int times[MAXN];
-int T[MAXN], lson[M], rson[M], c[M];
+int T[M], lson[M], rson[M], c[M];
 int seg_sum[M], seg_max[M];
 
 /**< 树链剖分 */
@@ -79,7 +78,7 @@ void push_up(int i) {
     seg_sum[i] = seg_sum[lson[i]] + seg_sum[rson[i]]);
 }
 void update(int l, int r, int root, int pos, int val) {
-    int newroot = ++tot, tmp = newroot;
+    int newroot = ++tot;
     int mid = (l + r) >> 1;
     if(l == r) {
         seg_sum[i] = val;
@@ -93,24 +92,31 @@ void update(int l, int r, int root, int pos, int val) {
         rson[newroot] = update(mid + 1, r, rson[root], k, val);
         lson[newroot] = lson[root];
     }
-
     push_up(newroot);
     return newroot;
 }
 
+int query(int l, int r, int x, int y, int root, int type) { /**< type==0 代表最大值， type==1 代表求和 */
+    if(l == x && r == y)
+        if(type == 0)
+            return seg_max[root];
+        else
+            return seg_sum[root];
+    int mid = (l + r) >> 1;
+    if(mid >= y)
+        return query(l, mid, x, y, lson[root], type);
+    else if(mid < x)
+        return query(mid + 1, r, x, y, rson[root], type);
+    else {
+        if(type == 0)
+            return max(query(l, mid, x, y, lson[root], type), query(mid + 1, r, x, y, rson[root], type));
+        if(type == 1)
+            return query(l, mid, x, y, lson[root], type) + query(mid + 1, r, x, y, rson[root], type);
+    }
 
-int query(int i, int l, int r) {
-    if(segTree[i].l == l && segTree[i].r == r)
-        return segTree[i].Max;
-    int mid = (segTree[i].l + segTree[i].r) / 2;
-    if(r <= mid)
-        return query(i << 1, l, r);
-    else if(l > mid)
-        return query((i << 1) | 1, l, r);
-    else
-        return max(query(i << 1, l, mid), query((i << 1) | 1, mid + 1, r));
+
 }
-int find(int u, int v) {
+int find(int u, int v, int type) {  /**< type==0 代表最大值， type==1 代表求和 */
     int f1 = top[u], f2 = top[v];
     int tmp = 0;
     while(f1 != f2) {
@@ -118,7 +124,10 @@ int find(int u, int v) {
             swap(f1, f2);
             swap(u, v);
         }
-        tmp = max(tmp, query(1, p[f1], p[u]));
+        if(type == 0)
+            tmp = max(tmp, query(0, pos - 1, p[f1], p[u], T[raw_city[u][0]], type));
+        else
+            tmp += query(0, pos - 1, p[f1], p[u], T[raw_city[u][0]], type);
         u = fa[f1];
         f1 = top[u];
     }
@@ -126,84 +135,12 @@ int find(int u, int v) {
         return tmp;
     if(deep[u] > deep[v])
         swap(u, v);
-    return max(tmp, query(1, p[son[u]], p[v]));
-}
+    if(type == 0)
+        return max(tmp, query(0, pos - 1, p[son[u]], p[v], T[raw_city[u][0]], type));
+    else
+        return tmp + query(0, pos - 1, p[son[u]], p[v], T[raw_city[u][0]], type)
+    }
 int e[MAXN]
-
-/**< 主席树 */
-int n, q, m, tot;
-int a[MAXN], t[MAXN];
-int times[MAXN];
-int T[MAXN], lson[M], rson[M], c[M];
-int seg_sum[M], seg_num[M];
-int build(int l, int r) {
-    int root = tot++;
-    seg_sum[root] = 0;
-    seg_num[root] = 0;
-    if(l != r) {
-        int mid = (l + r) >> 1;
-        lson[root] = build(l, mid);
-        rson[root] = build(mid + 1, r);
-    }
-    return root;
-}
-int update(int root, int pos, int val) {
-    int newroot = ++tot, tmp = newroot;
-    seg_sum[newroot] = seg_sum[root] + pos * val;
-    seg_num[newroot] = seg_num[root] + val;
-    seg_real[newroot] = seg_real[root] + reverse_rnk[pos] * val;
-    int l = 1, r = m;
-    while(l < r) {
-        int mid = (l + r) >> 1;
-        if(pos <= mid) {
-            lson[newroot] = ++tot;
-            rson[newroot] = rson[root];
-            newroot = lson[newroot];
-            root = lson[root];
-            r = mid;
-        } else {
-            rson[newroot] = ++tot;
-            lson[newroot] = lson[root];
-            newroot = rson[newroot];
-            root = rson[root];
-            l = mid + 1;
-        }
-        seg_sum[newroot] = seg_sum[root] + pos * val;
-        seg_num[newroot] = seg_num[root] + val;
-        seg_real[newroot] = seg_real[root] + reverse_rnk[pos] * val;
-    }
-    return tmp;
-}
-long long query(int root, int k) {
-    if(seg_num[root] < k) {
-        return  seg_real[root];
-    }
-    int l = 1, r = m;
-    long long res = 0;
-    while(l < r) {
-        int mid = (l + r) >> 1;
-        if(seg_num[lson[root]] >= k) {
-            r = mid;
-            root = lson[root];
-        } else {
-            res += seg_real[lson[root]];
-            k -= seg_num[lson[root]];
-            root = rson[root];
-            l = mid + 1;
-        }
-    }
-    if(l == r && k != 0)
-        res += reverse_rnk[l] * k;
-    return res;
-}
-void _copy(int x, int y) {
-    lson[x] = lson[y];
-    rson[x] = rson[y];
-    seg_sum[x] = seg_sum[y];
-    seg_num[x] = seg_num[y];
-    seg_real[x] = seg_real[y];
-}
-
 
 int city_num, event_num;
 int raw_city[100000 + 5][2];
@@ -229,34 +166,49 @@ void init() {
     dfs1(1, 0, 0);
     getpos(1, 1);
     /**< 建立线段树 */
-    build(1, 1, pos - 1);
+    build(0, pos - 1);
     T[0] = segTree[0];
-    for(int i = 1; i <= )
+    for(int i = 1; i <= MAXN; i++ ) {
+        T[i] = T[0];
     }
+    for(int i = 1; i <= city_num; i++) {
+        T[raw_city[1]] = update(0, pos - 1, T[raw_city[i][1]], p[i], raw_city[i][0]);
+    }
+}
 
 
 void work1() {
-    n = m = rnk_cnt;
-
-    T[0] = build(1, n);
-
-    int cur_mission = 2;
-    for(int i = 1; i <= time_num; i++) {
-        T[i] = ++tot;
-        _copy(T[i], T[i - 1]);
-        while(brk_missions[cur_mission].pos == i) {
-            T[i] = update(T[i], brk_missions[cur_mission].rnk, brk_missions[cur_mission].type);
-            cur_mission++;
+    for(int i = 1; i < event_num; i++) {
+        char tmp1, tmp2;
+        int city1, city2;
+        cin >> tmp;
+        cin >> tmp >> tmp1 >> tmp2 >> city1 >> city2;
+        if(tmp1 == 'Q' && tmp2 == 'S'){
+            cout<<find(city1, city2,1)<<endl;
+            continue;
+        }
+        else if(tmp1 == 'Q' && tmp2 == 'M'){
+            cout<<find(city1, city2,0)<<endl;
+            continue;
+        }
+        else if(tmp1 == 'Q' && tmp2 == 'M'){
+            cout<<find(city1, city2,0)<<endl;
+            continue;
+        }
+        else if(tmp1 == 'C' && tmp2 == 'C'){
+            update(0,pos-1,T[raw_city[city1][1]],p[city1],-raw_city[city1][0]);
+            raw_city[city1][1]=city2;
+            update(0,pos-1,T[raw_city[city1][1]],p[city1],raw_city[city1][0]);
+            continue;
+        }
+        else if(tmp1 == 'C' && tmp2 == 'W'){
+            update(0,pos-1,T[raw_city[city1][1]],p[city1],-raw_city[city1][0]);
+            raw_city[city1][0]=city2;
+            update(0,pos-1,T[raw_city[city1][1]],p[city1],raw_city[city1][0]);
+            continue;
         }
     }
 
-    int X, A, B, C;
-    long long res = 1, K;
-    for(int i = 1; i <= time_num; i++) {
-        cin >> X >> A >> B >> C;
-        K = 1 + (A * res + B) % C;
-        cout << (res = query(T[X], K)) << endl;
-    }
 }
 
 int main() {
