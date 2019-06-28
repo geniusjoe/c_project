7cfdc4f539e060a11d156be646b03cf3446727c0
geniusjoe <843520313@qq.com>
2019-06-28 10:27:44 +0800

    Update main.cpp

--- demon/main.cpp
+++ demon/main.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 #define MAXM     500000+5
+
 struct Trie {
     int next[1000010][26], fail[1000010], end[1000010];
     int root, L;
     int newnode() {
         for(int i = 0; i < 26; i++)
-            next[L][i] = -1;
+            next[L][i] = 1;
         end[L++] = 0;
         return L - 1;
-
     }
     void init() {
         L = 0;
@@ -22,27 +22,27 @@ struct Trie {
         int len = strlen(buf);
         int now = root;
         for(int i = 0; i < len; i++) {
-            if(next[now][buf[i] - 'a'] == -1)
+            if(next[now][buf[i] - 'a'] == 1)
                 next[now][buf[i] - 'a'] = newnode();
             now = next[now][buf[i] - 'a'];
         }
         end[now]++;
     }
     void build() {
-        queue<int> Q;
+        queue<int>Q;
         fail[root] = root;
         for(int i = 0; i < 26; i++)
-            if(next[root][i] == -1)
+            if(next[root][i] == 1)
                 next[root][i] = root;
             else {
                 fail[next[root][i]] = root;
                 Q.push(next[root][i]);
             }
-        while(!Q.empty()) {
+        while( !Q.empty() ) {
             int now = Q.front();
             Q.pop();
             for(int i = 0; i < 26; i++)
-                if(next[now][i] == -1)
+                if(next[now][i] == 1)
                     next[now][i] = next[fail[now]][i];
                 else {
                     fail[next[now][i]] = next[fail[now]][i];
@@ -57,7 +57,7 @@ struct Trie {
         for(int i = 0; i < len; i++) {
             now = next[now][buf[i] - 'a'];
             int temp = now;
-            while(temp != root) {
+            while( temp != root ) {
                 res += end[temp];
                 end[temp] = 0;
                 temp = fail[temp];
@@ -67,38 +67,60 @@ struct Trie {
     }
     void debug() {
         for(int i = 0; i < L; i++) {
-            printf("id = %3d,fail = %3d,end = %3d,chi = [", i, fail[i]
-                   , end[i]);
+            printf("id = %3d,fail = %3d,end = %3d,chi = [", i, fail[i],
+                   end[i]);
             for(int j = 0; j < 26; j++)
                 printf("%2d", next[i][j]);
             printf("]\n");
         }
     }
 };
-
 char buf[1000010];
-Trie    ac;
+Trie ac;
 
-int main() {
-
-#ifdef local
-    freopen("testdata.in", "r+", stdin);
-    freopen("testdata.out", "w+", stdout);
-#endif // local
-
-    //ios::sync_with_stdio(false);
-    int n;
-    scanf("%d", &n);
-    ac.init();
-    for(int i = 0; i < n; i++) {
-        scanf("%s", buf);
-        ac.insert(buf);
+void kmp_pre(char x[], int m, int next[]) {
+    int i, j;
+    j = next[0] = -1;
+    i = 0;
+    while(i < m) {
+        while(-1 != j && x[i] != x[j])
+            j = next[j];
+        next[++i] = ++j;
     }
-    ac.build();
-    scanf("%s", buf);
-    ac.debug();
-    printf("%d\n", ac.query(buf));
-    return 0;
+}
+int next[10010];
+int KMP_Count(char x[], int m, char y[], int n) {
+    int i, j;
+    int ans = 0;
+    kmp_pre(x, m, next);
+    i = j = 0;
+    while(i < n) {
+        while(-1 != j && y[i] != x[j])
+            j = next[j];
+        i++;
+        j++;
+        if(j >= m) {
+            ans++;
+            j = next[j];
+        }
+    }
+    return ans;
 }
 
-
+int main() {
+    int T;
+    int n;
+    scanf("%d", &T);
+    while( T-- ) {
+        scanf("%d", &n);
+        ac.init();
+        for(int i = 0; i < n; i++) {
+            scanf("%s", buf);
+            ac.insert(buf);
+        }
+        ac.build();
+        scanf("%s", buf);
+        printf("%d\n", ac.query(buf));
+    }
+    return 0;
+}
