f3e44f81d54fd14f59d21a3869934823d616bf45
geniusjoe <843520313@qq.com>
2019-06-29 11:50:24 +0800

    P3796 【模板】AC自动机（加强版）

--- demon/main.cpp
+++ demon/main.cpp
@@ -3,204 +3,101 @@
 #define debu
 using namespace std;
 
-#define MAXM    500000+5
+#define MAXM     500000+5
+#define INF 0x3f3f3f3f
 
-const int MAXN = 500025;
-const int M = MAXN * 50;
+char tmplate[250][2500];
+char buf[1000010];
+int hash[1000010], tot = 0;
+int res[1000010];
+int T, n;
 
-/**< 树链剖分 */
-struct Edge {
-    int to, next;
-} edge[MAXN * 2];
-int head[MAXN], TOT;
-int top[MAXN];
-int fa[MAXN];
-int deep[MAXN];
-int num[MAXN];
-int p[MAXN];
-int fp[MAXN];
-int son[MAXN];
-int pos;
-
-/**< 主席树 */
-int n, q, m, tot;
-int a[MAXN], t[MAXN];
-int times[MAXN];
-int T[M], lson[M], rson[M], c[M];
-int seg_sum[M], seg_max[M];
-int city_num, event_num;
-int raw_city[100000 + 5][2];
-
-/**< 树链剖分 */
-void addedge(int u, int v) {
-    edge[TOT].to = v;
-    edge[TOT].next = head[u];
-    head[u] = TOT++;
-}
-void dfs1(int u, int pre, int d) {
-    deep[u] = d;
-    fa[u] = pre;
-    num[u] = 1;
-    for(int i = head[u]; i != -1; i = edge[i].next) {
-        int v = edge[i].to;
-        if(v != pre) {
-            dfs1(v, u, d + 1);
-            num[u] += num[v];
-            if(son[u] == -1 || num[v] > num[son[u]])
-                son[u] = v;
+struct Trie {
+    int next[1000010][26], fail[1000010], end[1000010];
+    int root, L;
+    int newnode() {
+        for(int i = 0; i < 26; i++)
+            next[L][i] = -1;
+        end[L++] = 0;
+        return L - 1;
+    }
+    void init() {
+        L = 0;
+        root = newnode();
+        memset(hash,0,sizeof(hash));
+        memset(res,0,sizeof(res));
+    }
+    void insert(char buf[], int order) {
+        int len = strlen(buf);
+        int now = root;
+        for(int i = 0; i < len; i++) {
+            if(next[now][buf[i] - 'a'] == -1)
+                next[now][buf[i] - 'a'] = newnode();
+            now = next[now][buf[i] - 'a'];
+        }
+        end[now] = order;
+    }
+    void build() {
+        queue<int>Q;
+        fail[root] = root;
+        for(int i = 0; i < 26; i++)
+            if(next[root][i] == -1)
+                next[root][i] = root;
+            else {
+                fail[next[root][i]] = root;
+                Q.push(next[root][i]);
+            }
+        while( !Q.empty() ) {
+            int now = Q.front();
+            Q.pop();
+            for(int i = 0; i < 26; i++)
+                if(next[now][i] == -1)
+                    next[now][i] = next[fail[now]][i];
+                else {
+                    fail[next[now][i]] = next[fail[now]][i];
+                    Q.push(next[now][i]);
+                }
         }
     }
-}
-void getpos(int u, int sp) {
-    top[u] = sp;
-    p[u] = pos++;
-    fp[p[u]] = u;
-    if(son[u] == -1)
-        return ;
-    getpos(son[u], sp);
-    for(int i = head[u]; i != -1; i = edge[i].next) {
-        int v = edge[i].to;
-        if(v != son[u] && v != fa[u])
-            getpos(v, v);
-    }
-}
-int build(int l, int r) {
-    int root = tot++;
-    seg_sum[root] = 0;
-    seg_max[root] = 0;
-    if(l != r) {
-        int mid = (l + r) >> 1;
-        lson[root] = build(l, mid);
-        rson[root] = build(mid + 1, r);
-    }
-    return root;
-}
-void push_up(int i) {
-    seg_max[i] = max(seg_max[lson[i]], seg_max[rson[i]]);
-    seg_sum[i] = seg_sum[lson[i]] + seg_sum[rson[i]];
-}
-int update(int l, int r, int root, int pos, int val) {
-    int newroot = ++tot;
-    int mid = (l + r) >> 1;
-    if(l == r) {
-        seg_sum[newroot] = seg_sum[root] + val;
-        seg_max[newroot] = seg_max[root] + val;
-        return newroot;
-    }
-    if(pos <= mid) {
-        lson[newroot] = update(l, mid, lson[root], pos, val);
-        rson[newroot] = rson[root];
-    } else {
-        rson[newroot] = update(mid + 1, r, rson[root], pos, val);
-        lson[newroot] = lson[root];
-    }
-    push_up(newroot);
-    return newroot;
-}
-
-int query(int l, int r, int x, int y, int root, int type) { /**< type==0 代表最大值， type==1 代表求和 */
-    if(l >= x && r <= y) {
-        if(type == 0)
-            return seg_max[root];
-        else
-            return seg_sum[root];
-    }
-
-    int mid = (l + r) >> 1;
-    if(mid >= y)
-        return query(l, mid, x, y, lson[root], type);
-    else if(mid < x)
-        return query(mid + 1, r, x, y, rson[root], type);
-    else {
-        if(type == 0)
-            return max(query(l, mid, x, y, lson[root], type), query(mid + 1, r, x, y, rson[root], type));
-        if(type == 1)
-            return query(l, mid, x, y, lson[root], type) + query(mid + 1, r, x, y, rson[root], type);
-    }
-}
-int find(int u, int v, int type, int k) { /**< type==0 代表最大值， type==1 代表求和 */
-    int f1 = top[u], f2 = top[v];
-    int tmp = 0;
-    while(f1 != f2) {
-        if(deep[f1] < deep[f2]) {
-            swap(f1, f2);
-            swap(u, v);
+    int query(char buf[]) {
+        int len = strlen(buf);
+        int now = root;
+        for(int i = 0; i < len; i++) {
+            now = next[now][buf[i] - 'a'];
+            int temp = now;
+            while( temp != root ) {
+                hash[end[temp]]++;
+                temp = fail[temp];
+            }
         }
-        if(type == 0)
-            tmp = max(tmp, query(0, pos - 1, p[f1], p[u], T[raw_city[k][1]], type));
-        else
-            tmp += query(0, pos - 1, p[f1], p[u], T[raw_city[k][1]], type);
-        u = fa[f1];
-        f1 = top[u];
-    }
-    //if(u == v)
-     //   return seg_sum[p[u]];
-    if(deep[u] > deep[v])
-        swap(u, v);
-    if(type == 0)
-        return max(tmp, query(0, pos - 1, p[u], p[v], T[raw_city[k][1]], type));
-    else
-        return tmp + query(0, pos - 1, p[u], p[v], T[raw_city[k][1]], type);
-}
-int e[MAXN];
-
-
-void init() {
-    TOT = tot = 0;
-    memset(head, -1, sizeof(head));
-    pos = 0;
-    memset(son, -1, sizeof(son));
-
-    cin >> city_num >> event_num;
-
-
-    for(int i = 1; i <= city_num; i++) {
-        cin >> raw_city[i][0] >> raw_city[i][1];
-    }
-    for(int i = 1; i <= city_num - 1; i++) {
-        int u, v;
-        cin >> u >> v;
-        addedge(u, v);
-        addedge(v, u);
-    }
-    dfs1(1, 0, 0);
-    getpos(1, 1);
-    T[0] = build(0, pos - 1);
-
-    for(int i = 1; i <= MAXN; i++ ) {
-        T[i] = T[0];
-    }
-    for(int i = 1; i <= city_num; i++) {
-        T[raw_city[i][1]] = update(0, pos - 1, T[raw_city[i][1]], p[i], raw_city[i][0]);
-    }
-}
-
-
-void work1() {
-    for(int i = 1; i <= event_num; i++) {
-        char tmp1, tmp2;
-        int city1, city2;
-        cin >> tmp1 >> tmp2 >> city1 >> city2;
-        if(tmp1 == 'Q' && tmp2 == 'S') {
-            cout << find(city1, city2, 1, city1) << endl;
-            continue;
-        } else if(tmp1 == 'Q' && tmp2 == 'M') {
-            cout << find(city1, city2, 0, city1) << endl;
-            continue;
-        } else if(tmp1 == 'C' && tmp2 == 'C') {
-            T[raw_city[city1][1]] = update(0, pos - 1, T[raw_city[city1][1]], p[city1], -raw_city[city1][0]);
-            raw_city[city1][1] = city2;
-            T[raw_city[city1][1]] = update(0, pos - 1, T[raw_city[city1][1]], p[city1], raw_city[city1][0]);
-            continue;
-        } else if(tmp1 == 'C' && tmp2 == 'W') {
-            int tmp = raw_city[city1][0];
-            raw_city[city1][0] = city2;
-            T[raw_city[city1][1]] = update(0, pos - 1, T[raw_city[city1][1]], p[city1], -tmp + raw_city[city1][0]);
-            continue;
+        int cnt = -1, max = -INF;
+        for(int i = 1; i <= n; i++) {
+            if(hash[i] == max)
+                res[++cnt] = i;
+            else if(hash[i] > max)    {
+                max = hash[i];
+                cnt = 0;
+                res[cnt] = i;
+            } else
+                continue;
+        }
+        cout << max << endl;
+        for(int i = 0; i <= cnt; i++) {
+            printf("%s\n", tmplate[res[i]]);
         }
     }
+    void debug() {
+        for(int i = 0; i < L; i++) {
+            printf("id = %3d,fail = %3d,end = %3d,chi = [", i, fail[i],
+                   end[i]);
+            for(int j = 0; j < 26; j++)
+                printf("%2d", next[i][j]);
+            printf("]\n");
+        }
+    }
+};
 
-}
+Trie ac;
 
 int main() {
 
@@ -209,10 +106,17 @@ int main() {
     freopen("testdata.out", "w+", stdout);
 #endif // local
 
-    ios::sync_with_stdio(false);
 
-    init();
-
-    work1();
-
+    while(scanf("%d", &n) && n != 0) {
+        ac.init();
+        for(int i = 1; i <= n; i++) {
+            scanf("%s", tmplate[i]);
+            ac.insert(tmplate[i], i);
+        }
+        ac.build();
+        scanf("%s", buf);
+        ac.query(buf);
+    }
+    return 0;
 }
+
