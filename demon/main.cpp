9b216e0e53f61d8c9f60e591df676ba2355e841c
geniusjoe <843520313@qq.com>
2019-06-29 16:27:23 +0800

    2015 Facebook Hacker Cup C

--- demon/main.cpp
+++ demon/main.cpp
@@ -1,131 +1,81 @@
 #include<bits/stdc++.h>
-#define local
+#define loca
 #define debu
 using namespace std;
 
 #define MAXM     500000+5
 #define INF 0x3f3f3f3f
 
+const int model = 1000000007;
+
 char tmplate[250][2500];
 char buf[1000010];
+long long base1[3000][3000];
+long long base2[3000][3000];
 int tot = 0, res = 0;
 int T, n;
 
-struct Trie {
-    int next[1000010][26], fail[1000010], end[1000010];
-    int root, L;
-    int newnode() {
-        for(int i = 0; i < 26; i++)
-            next[L][i] = -1;
-        end[L++] = 0;
-        return L - 1;
+long long  work1(int u, int t, int U, int T) {
+#ifdef debug
+    cout << u << "\t" << t << endl;
+#endif // debug
+    if(u == U && t == T)
+        return 1;
+    else if(u <= t)
+        return 0;
+    else if(u == U && t < T)
+        return 1;
+    else if(t == T && u > T)
+        return 1;
+    else if(t == T && u <= T)
+        return 0;
+    else {
+        if(base1[u + 1][t] == -1)
+            base1[u + 1][t] = work1(u + 1, t, U, T) % model;
+        if(base1[u][t + 1] == -1)
+            base1[u][t + 1] = work1(u, t + 1, U, T) % model;
+        return base1[u + 1][t] + base1[u][t + 1] % model;
     }
-    void init() {
-        L = 0;
-        root = newnode();
-        res = 0;
-    }
-    void insert(char buf[], int order) {
-        int len = strlen(buf);
-        int now = root;
-        for(int i = 0; i < len; i++) {
-            if(next[now][buf[i] - 'a'] == -1) {
-                next[now][buf[i] - 'a'] = newnode();
-            } else {
-                res++;
-                now = next[now][buf[i] - 'a'];
-            }
-        }
-        end[now]++;
-    }
-    /*
-    void build() {
-        queue<int>Q;
-        fail[root] = root;
-        for(int i = 0; i < 26; i++)
-            if(next[root][i] == -1)
-                next[root][i] = root;
-            else {
-                fail[next[root][i]] = root;
-                Q.push(next[root][i]);
-            }
-        while( !Q.empty() ) {
-            int now = Q.front();
-            Q.pop();
-            for(int i = 0; i < 26; i++)
-                if(next[now][i] == -1)
-                    next[now][i] = next[fail[now]][i];
-                else {
-                    fail[next[now][i]] = next[fail[now]][i];
-                    Q.push(next[now][i]);
-                }
-        }
-    }
-    */
-    /*
-    int query(char buf[]) {
-        int len = strlen(buf);
-        int now = root;
-        for(int i = 0; i < len; i++) {
-            now = next[now][buf[i] - 'a'];
-            int temp = now;
-            while( temp != root ) {
-                hash[end[temp]]++;
-                temp = fail[temp];
-            }
-        }
-        int cnt = -1, max = -INF;
-        for(int i = 1; i <= n; i++) {
-            if(hash[i] == max)
-                res[++cnt] = i;
-            else if(hash[i] > max)    {
-                max = hash[i];
-                cnt = 0;
-                res[cnt] = i;
-            } else
-                continue;
-        }
-        cout << max << endl;
-        for(int i = 0; i <= cnt; i++) {
-            printf("%s\n", tmplate[res[i]]);
-        }
-    }
-    */
-    void debug() {
-        for(int i = 0; i < L; i++) {
-            printf("id = %3d,fail = %3d,end = %3d,chi = [", i, fail[i],
-                   end[i]);
-            for(int j = 0; j < 26; j++)
-                printf("%2d", next[i][j]);
-            printf("]\n");
-        }
-    }
-};
+}
 
-Trie ac;
+long long work2(int u, int t, int U, int T) {
+    if(u == U && t == T)
+        return 1;
+    else if(t < T && u > t)
+        return 0;
+    else if(t > T || u > U)
+        return 0;
+    else if(t == T && u > T)
+        return 0;
+    else if(t == T && u <= T)
+        return 1;
+    else {
+        if(base2[u + 1][t] == -1)
+            base2[u + 1][t] = work2(u + 1, t, U, T) % model;
+        if(base2[u][t + 1] == -1)
+            base2[u][t + 1] = work2(u, t + 1, U, T) % model;
+        return base2[u + 1][t] + base2[u][t + 1] % model;
+    }
+}
+
 
 int main() {
-
 #ifdef local
     freopen("testdata.in", "r+", stdin);
     freopen("testdata.out", "w+", stdout);
 #endif // local
 
-    int T;
-    scanf("%d", &T);
-    for(int k = 1; k <= T; k++) {
-        cin >> n;
-        ac.init();
-        for(int i = 1; i <= n; i++) {
-            scanf("%s", tmplate[i]);
-            ac.insert(tmplate[i], i);
-        }
-        cout << "Case #" << k << ": " << res << endl;
-
-        //ac.build();
-        //scanf("%s", buf);
-        //ac.query(buf);
-
+    int n;
+    scanf("%d", &n);
+    for(int k = 1; k <= n; k++) {
+        memset(base1, -1, sizeof(base1));
+        memset(base2, -1, sizeof(base2));
+        int U, T;
+        char hyphen;
+        cin >> U >> hyphen >> T;
+        long long res1 = work1(1, 0, U, T);
+        long long res2 = work2(0, 0, U, T);
+        cout << "Case #" << k << ": " << res1 << " " << res2 << endl;
     }
     return 0;
 }
