0a4fc66f5e514231698824fb046b50050e814550
geniusjoe <843520313@qq.com>
2019-06-30 18:01:37 +0800

    P2444 [POI2000]病毒

--- demon/main.cpp
+++ demon/main.cpp
@@ -6,14 +6,14 @@ using namespace std;
 #define MAXM     500000+5
 #define INF 0x3f3f3f3f
 
-char tmplate[250][2500];
 char buf[1000010];
 int hash[1000010], tot = 0;
 int res[1000010];
+bool vis[1000010], ins[1000010], flag;
 int T, n;
 
 struct Trie {
-    int next[1000010][26], fail[1000010], end[1000010];
+    int next[10000100][2], fail[10000100], end[10000100];
     int root, L;
     int newnode() {
         for(int i = 0; i < 26; i++)
@@ -24,23 +24,23 @@ struct Trie {
     void init() {
         L = 0;
         root = newnode();
-        memset(hash,0,sizeof(hash));
-        memset(res,0,sizeof(res));
+        memset(hash, 0, sizeof(hash));
+        memset(res, 0, sizeof(res));
     }
-    void insert(char buf[], int order) {
+    void insert(char buf[]) {
         int len = strlen(buf);
         int now = root;
         for(int i = 0; i < len; i++) {
-            if(next[now][buf[i] - 'a'] == -1)
-                next[now][buf[i] - 'a'] = newnode();
-            now = next[now][buf[i] - 'a'];
+            if(next[now][buf[i] - '0'] == -1)
+                next[now][buf[i] - '0'] = newnode();
+            now = next[now][buf[i] - '0'];
         }
-        end[now] = order;
+        end[now] = 1;
     }
     void build() {
         queue<int>Q;
         fail[root] = root;
-        for(int i = 0; i < 26; i++)
+        for(int i = 0; i < 2; i++)
             if(next[root][i] == -1)
                 next[root][i] = root;
             else {
@@ -50,47 +50,38 @@ struct Trie {
         while( !Q.empty() ) {
             int now = Q.front();
             Q.pop();
-            for(int i = 0; i < 26; i++)
+            for(int i = 0; i < 2; i++)
                 if(next[now][i] == -1)
                     next[now][i] = next[fail[now]][i];
                 else {
                     fail[next[now][i]] = next[fail[now]][i];
+                    if(end[next[now][i]] == 0)
+                        end[next[now][i]] = end[next[fail[now]][i]];
                     Q.push(next[now][i]);
                 }
         }
     }
-    int query(char buf[]) {
-        int len = strlen(buf);
-        int now = root;
-        for(int i = 0; i < len; i++) {
-            now = next[now][buf[i] - 'a'];
-            int temp = now;
-            while( temp != root ) {
-                hash[end[temp]]++;
-                temp = fail[temp];
-            }
+
+    inline void dfs(int x) {
+        if(ins[x]) {
+            printf("TAK\n");
+            exit(0);
         }
-        int cnt = -1, max = -INF;
-        for(int i = 1; i <= n; i++) {
-            if(hash[i] == max)
-                res[++cnt] = i;
-            else if(hash[i] > max)    {
-                max = hash[i];
-                cnt = 0;
-                res[cnt] = i;
-            } else
-                continue;
-        }
-        cout << max << endl;
-        for(int i = 0; i <= cnt; i++) {
-            printf("%s\n", tmplate[res[i]]);
-        }
+        if(vis[x] || end[x])
+            return;
+        vis[x] = ins[x] = 1;
+        if(next[x][0])
+            dfs(next[x][0]);
+        if(next[x][1])
+            dfs(next[x][1]);
+        ins[x] = 0;
     }
+
     void debug() {
         for(int i = 0; i < L; i++) {
             printf("id = %3d,fail = %3d,end = %3d,chi = [", i, fail[i],
                    end[i]);
-            for(int j = 0; j < 26; j++)
+            for(int j = 0; j < 2; j++)
                 printf("%2d", next[i][j]);
             printf("]\n");
         }
@@ -107,16 +98,21 @@ int main() {
 #endif // local
 
 
-    while(scanf("%d", &n) && n != 0) {
-        ac.init();
-        for(int i = 1; i <= n; i++) {
-            scanf("%s", tmplate[i]);
-            ac.insert(tmplate[i], i);
-        }
-        ac.build();
+    scanf("%d", &n);
+    ac.init();
+    for(int i = 1; i <= n; i++) {
         scanf("%s", buf);
-        ac.query(buf);
+        ac.insert(buf);
     }
+    ac.build();
+    //ac.debug();
+    ac.dfs(0);
+
+    if(flag == true)
+        cout << "TAK" << endl;
+    else
+        cout << "NIE" << endl;
+
     return 0;
 }
 
