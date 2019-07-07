91bb52f676bcffe36d2ac75efff9e98a69d3494e
geniusjoe <843520313@qq.com>
2019-07-07 19:20:53 +0800

    Update main.cpp

--- demon/main.cpp
+++ demon/main.cpp
@@ -22,14 +22,12 @@ int fp[MAXN];
 int son[MAXN];
 int pos;
 
-/**< 主席树 */
 int n, q, m, tot;
 int a[MAXN], t[MAXN];
 int times[MAXN];
-int T[M], lson[M], rson[M], c[M];
-int seg_sum[M], seg_max[M];
-int city_num, event_num;
-int raw_city[MAXN];
+int seg_sum[M];
+int l_cor[M],r_cor[M];
+int cor_cnt=10000000;
 
 /**< 树链剖分 */
 void addedge(int u, int v) {
@@ -82,26 +80,33 @@ inline long long rchild(long long x) {
 }
 inline void push_up(long long p) {
     ans[p]=(ans[lchild(p)]+ans[rchild(p)]);
+    l_cor[p]=l_cor[lchild(p)];
+    r_cor[p]=r_cor[rchild(p)];
+    add[p]=0;
+    if(r_cor[lchild(p)]==l_cor[rchild(p)])  ans[p]--;
 }
 
 /**< 由顶向下建立线段树 */
 /**< 左右均为闭区间 */
-void build(long long p,long long l,long long r) {
+void build(long long p,long long l,long long r,int val) {
     add[p]=0;
     if(l==r) {
-        ans[p]=a[l];
+        ans[p]=1;
+        l_cor[p]=val;
+        r_cor[p]=val;
         return ;
     }
     long long mid=(l+r)>>1;
-    build(lchild(p),l,mid);
-    build(rchild(p),mid+1,r);
+    build(lchild(p),l,mid,val);
+    build(rchild(p),mid+1,r,val);
     push_up(p);
 }
 inline void f(long long p,long long l,long long r,
-              long long cur_add) {
-
-    add[p]+=cur_add;
-    ans[p]=ans[p]+ cur_add*(r-l+1);
+              long long cor) {
+    if(cor>0){
+        add[p]=cor;
+        ans[p]=1;
+    }
 }
 inline void push_down(long long p,long long l,long long r) {
     long long mid=(l+r)>>1;
@@ -112,116 +117,50 @@ inline void push_down(long long p,long long l,long long r) {
 /**< 区间赋值操作 */
 /**< 区间内元素增加的值 */
 inline void update(long long nl,long long nr,   /**< 目标边界 */
-                   long long l,long long r,long long p /**< 当前边界和节点 */
+                   long long l,long long r,long long p,int val /**< 当前边界和节点 */
                   ) {
-    if(opt_type==2&&nl<=l&&r<=nr) {
-        ans[p]=(ans[p]+ add_num *(r-l+1))%model;
-        add[p]=(add[p]+ add_num )%model;
-        return ;
-    }
-    if(opt_type==1&&nl<=l&&r<=nr) {
-        ans[p]=(ans[p]* mul_num )%model;
-        add[p]=(add[p]* mul_num )%model;
-        mul[p]=(mul[p]* mul_num )%model;
+    if(nl<=l&&r<=nr) {
+        ans[p]=1;
+        add[p]=val;
+        l_cor[p]=val;
+        r_cor[p]=val;
         return ;
     }
     push_down(p,l,r);
     long long mid=(l+r)>>1;
     if(nl<=mid)
-        update(nl,nr,l,mid,lchild(p));
+        update(nl,nr,l,mid,lchild(p),val);
     if(nr>mid)
-        update(nl,nr,mid+1,r,rchild(p));
+        update(nl,nr,mid+1,r,rchild(p),val);
     push_up(p);
 }
 /**< 区间查询操作 */
 long long query(long long q_x,long long q_y,long long l,long long r,long long p) {
     long long res=0;
     if(q_x<=l&&r<=q_y)
-        return ans[p]%model;
+        return ans[p];
     long long mid=(l+r)>>1;
     push_down(p,l,r);
     if(q_x<=mid)
         res+=query(q_x,q_y,l,mid,lchild(p));
     if(q_y>mid)
         res+=query(q_x,q_y,mid+1,r,rchild(p));
-    return res%model;
+    return res;
 }
-
-
-int build(int l, int r) {
-    int root = tot++;
-    seg_sum[root] = 0;
-    seg_max[root] = 0;
-    if(l != r) {
-        int mid = (l + r) >> 1;
-        lson[root] = build(l, mid);
-        rson[root] = build(mid + 1, r);
-    }
-    return root;
-}
-void push_up(int i) {
-    seg_max[i] = max(seg_max[lson[i]], seg_max[rson[i]]);
-    seg_sum[i] = seg_sum[lson[i]] + seg_sum[rson[i]];
-}
-int update(int l, int r, int root, int pos, int val) {
-    int newroot = ++tot;
-    int mid = (l + r) >> 1;
-    if(l == r) {
-        seg_sum[newroot] = seg_sum[root] + val;
-        seg_max[newroot] = seg_max[root] + val;
-        return newroot;
-    }
-    if(pos <= mid) {
-        lson[newroot] = update(l, mid, lson[root], pos, val);
-        rson[newroot] = rson[root];
-    } else {
-        rson[newroot] = update(mid + 1, r, rson[root], pos, val);
-        lson[newroot] = lson[root];
-    }
-    push_up(newroot);
-    return newroot;
-}
-
-int query(int l, int r, int x, int y, int root, int type) { /**< type==0 代表最大值， type==1 代表求和 */
-    if(l >= x && r <= y) {
-        if(type == 0)
-            return seg_max[root];
-        else
-            return seg_sum[root];
-    }
-
-    int mid = (l + r) >> 1;
-    if(mid >= y)
-        return query(l, mid, x, y, lson[root], type);
-    else if(mid < x)
-        return query(mid + 1, r, x, y, rson[root], type);
-    else {
-        if(type == 0)
-            return max(query(l, mid, x, y, lson[root], type), query(mid + 1, r, x, y, rson[root], type));
-        if(type == 1)
-            return query(l, mid, x, y, lson[root], type) + query(mid + 1, r, x, y, rson[root], type);
-    }
-}
-int find(int u, int v, int type, int k) { /**< type==0 代表最大值， type==1 代表求和 */
+int find(int u, int v, int k) {
     int f1 = top[u], f2 = top[v];
-    int tmp0 = 0,tmp1=-100000;
+    int tmp0 = 0;
     while(f1 != f2) {
         if(deep[f1] < deep[f2]) {
             swap(f1, f2);
             swap(u, v);
         }
-        if(type == 0)
-            tmp1 = max(tmp1, query(0, pos - 1, p[f1], p[u], T[0], type));
-        else
-            tmp0 += query(0, pos - 1, p[f1], p[u], T[0], type);
+        tmp0 += query(0, pos - 1, p[f1], p[u], T[0], type);
         u = fa[f1];
         f1 = top[u];
     }
     if(deep[u] > deep[v])
         swap(u, v);
-    if(type == 0)
-        return max(tmp1, query(0, pos - 1, p[u], p[v], T[0], type));
-    else
         return tmp0 + query(0, pos - 1, p[u], p[v], T[0], type);
 }
 int e[MAXN];
