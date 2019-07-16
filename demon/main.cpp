774a5fa22404b4f29b6905f96397da38525eee6a
Geniusjoe <843520313@qq.com>
2019-07-16 11:29:38 +0800

    modify

--- demon/main.cpp
+++ demon/main.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-#define local
+#define loca
 #define debu
 using namespace std;
 
@@ -7,14 +7,17 @@ const int MAXN = 100010;
 const int M = MAXN * 30;
 int n, q,m, tot;
 int a[MAXN], t[MAXN];
-int T[MAXN], lson[M], rson[M], c[M];
+int lson[M], rson[M], c[M];
+int T[MAXN],tme_buf[MAXN];
 
 int q_cnt, tme_cnt;
-struct time {       /**< type==1 为开始    type==2为结束  type==3为查询 */
-    int type, date, val;
-    bool operator < (time time1)
-    return this->date < time1.date;
-} times[MAXN];
+struct timing{
+    int type,date,val;
+    bool operator <(timing time1)
+    {
+        return this->date<time1.date;
+    }
+}times[MAXN];
 
 struct que {
     int q_time, k;
@@ -22,30 +25,35 @@ struct que {
 
 
 void init() {
-    scanf("%d", &q_num);
-    int cur_type;
-    scanf("%d", &cur_type);
-    if(cur_type == 1) {
-        int start, finish, val;
-        scanf("%d%d%d", &start, &val, &finish);
-        times[++tme_cnt].date = start;
-        times[tme_cnt].type = 1;
-        times[tme_cnt].val = val;
-        times[++tme_cnt].date = finish + 1;
-        times[tme_cnt].type = 2;
-        times[tme_cnt].val = val;
-    } else {
-        int cur_time, cur_k;
-        scanf("%d%d", &cur_time, &cur_k);
-        ques[++q_cnt].q_time = cur_time;
-        ques[q_cnt].k = cur_k;
-        times[++tme_cnt].date = cur_time;
-        times[++tme_cnt].type = 3;
+    int cur_num;
+    tme_cnt=q_cnt=0;
+    cin>>cur_num;
+    for(int i=1; i<=cur_num; i++) {
+        int cur_type;
+        cin>>cur_type;
+        if(cur_type == 1) {
+            int start, finish, val;
+            cin>>start>>val>>finish;
+            times[++tme_cnt].date = start;
+            times[tme_cnt].type = 1;
+            times[tme_cnt].val = val;
+            times[++tme_cnt].date = finish + 1;
+            times[tme_cnt].type = 2;
+            times[tme_cnt].val = val;
+        } else {
+            int cur_time, cur_k;
+            cin>>cur_time>>cur_k;
+            ques[++q_cnt].q_time = cur_time;
+            ques[q_cnt].k = cur_k;
+            times[++tme_cnt].date = cur_time;
+            times[tme_cnt].type = 3;
+        }
 
-    sort(times + 1, times + 1 + tme_cnt);
-
-    m = unique(times + 1, times + 1 + tme_cnt)−times−1;
-
+        sort(times + 1, times + 1 + tme_cnt);
+        for(int i=1;i<=tme_cnt;i++)
+            tme_buf[i]=times[i].date;
+        m = unique(tme_buf+1,tme_buf+1+tme_cnt)-tme_buf-1;
+    }
 }
 
 int build(int l, int r) {
@@ -58,8 +66,8 @@ int build(int l, int r) {
     }
     return root;
 }
-int hash(int x) {
-        return lower_bound(times + 1, times + 1 + m0, x) − times;
+int Hash(int x) {
+    return lower_bound(tme_buf + 1, tme_buf + 1 + tme_cnt, x) - tme_buf;
 }
 int update(int root, int pos, int val) {
     int newroot = tot++, tmp = newroot;
@@ -84,21 +92,19 @@ int update(int root, int pos, int val) {
     }
     return tmp;
 }
-int query(int left_root, int right_root, int k) {
+int query(int root, int k) {
     int l = 1, r = m;
-    if(c[left_root]-c[right_root]<k)
+    if(c[root]<k)
         return -1;
     while( l < r) {
         int mid = (l + r) >> 1;
-        if(c[lson[left_root]]−c[lson[right_root]] >= k ) {
+        if(c[lson[root]] >= k ) {
             r = mid;
-            left_root = lson[left_root];
-            right_root = lson[right_root];
+            root = lson[root];
         } else {
             l = mid + 1;
-            k − = c[lson[left_root]] − c[lson[right_root]];
-            left_root = rson[left_root];
-            right_root = rson[right_root];
+            k -= c[lson[root]];
+            root = rson[root];
         }
     }
     return l;
@@ -109,39 +115,39 @@ int main() {
     freopen("testdata.out", "w+", stdout);
 #endif // local
 
-    int T;
-    scanf("%d", &T);
-    while(T--) {
-        tot=0;
+    ios::sync_with_stdio(false);
 
+
+    int k;
+    cin>>k;
+    for(int ii=1;ii<=k;ii++) {
+        cout<<"Case "<<ii<<":"<<endl;
+        tot=0;
         init();
         int cnt1=1;
 
-        T[0] = build(1, 1000000+5);
+        T[0]=build(1,1000000+5);
 
         for(int i = 1; i <=m; i++) {
             T[i]=T[i-1];
-            while(hash(times[cnt1].date)==i){
+            while(Hash(times[cnt1].date)==i) {
                 if(times[cnt1].type==1)
-                    update(T[i],times[cnt1].val,1);
+                    T[i]=update(T[i],times[cnt1].val,1);
                 else if(times[cnt1].type==2)
-                    update(T[i],times[cnt1].val,-1);
+                    T[i]=update(T[i],times[cnt1].val,-1);
                 cnt1++;
             }
         }
 
-
-        for(int i=1;i<=q_cnt;i++){
-            int num=hash(ques[i].q_time);
-            cout<<query(T[num],T[num-1],ques[i].k)<<endl;
+        for(int i=1; i<=q_cnt; i++) {
+            int num=Hash(ques[i].q_time);
+            cout<<query(T[num],ques[i].k)<<endl;
         }
-
     }
-
 
     return 0;
 }
 
 
 
-}
+
