9abb109556388c1254f5d8a0d073b0cc8cfd47a3
geniusjoe <843520313@qq.com>
2019-07-21 09:48:37 +0800

    modify

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -1,42 +1,50 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-#define local
+#define loc
 
-const int maxn=1000;
+const int maxn = 1000;
+bool vis[28 + 5];
 
 int buf[maxn][maxn];
 int n;
 long long ans;
 
 
-void dfs(int x,int t,long long Sum) {
-    if(x==n){
-        ans=max(ans,Sum);
-        return ;
+void dfs(int x, int t, long long Sum) {
+    if (x == 2 * n) {
+        ans = max(ans, Sum);
+        return;
     }
-    if(t<n){
-
+    if (x - t < n) {
+        vis[x + 1] = false;
+        dfs(x + 1, t, Sum);
     }
-    if(n-x>t){
-        
+    if (t < n) {
+        vis[x + 1] = true;
+        for (int i = 1; i <= 2 * n; i++) {
+            if (vis[i]) Sum -= buf[i][x + 1];
+            else Sum += buf[i][x + 1];
+        }
+        dfs(x + 1, t + 1, Sum);
     }
-
 }
 
-int main()
-{
+int main() {
 #ifdef local
-    freopen("testdata.in","r+",stdin);
-    freopen("testdata.out","w+",stdout);
-    #endif
+    freopen("testdata.in", "r+", stdin);
+    freopen("testdata.out", "w+", stdout);
+#endif
 
     ios::sync_with_stdio(false);
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cin>>buf[i][j];
+
+    cin >> n;
+    for (int i = 1; i <= 2 * n; i++) {
+        for (int j = 1; j <= 2 * n; j++) {
+            cin >> buf[i][j];
         }
     }
-    dfs(1,1,ans);
+    dfs(0, 0, ans);
+    cout << ans << endl;
 }
