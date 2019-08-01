662bbbfb19d81c2484c7da55c293d9b113fd9bb8
geniusjoe <843520313@qq.com>
2019-08-01 14:03:39 +0800

    Codeforces Round #573 (Div. 2) B

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -1,79 +1,70 @@
-#include <bits/stdc++.h>
-
-#define loc
-const int INF = 0x3f3f3f3f;
+#include<bits/stdc++.h>
+#define loca
+#define debu
 using namespace std;
-const long long maxn = 2e5 + 5;
 
-long long n, res = 0, cnt;
-long long sz[maxn + 5], dp[maxn + 5];
+const int MAXN=500010;
 
-int head[maxn + 5]; // 当前点的第一条边
-struct road {
-    int to, next;
-} roads[2 * maxn + 5];
-
-
-void add(int a, int b) {
-    cnt++;
-    roads[cnt].to = b;
-    roads[cnt].next = head[a];
-    head[a] = cnt;
-}
-
-long long dfs1(int p, int i) {
-    sz[i] = 1;
-    for (int cur_road = head[i]; cur_road; cur_road = roads[cur_road].next) {
-        int j = roads[cur_road].to;
-        if (j == p) continue;
-        sz[i] += dfs1(i, j);
-    }
-    return sz[i];
-}
-
-long long dfs2(int p, int i) {
-    for (int cur_road = head[i]; cur_road; cur_road = roads[cur_road].next) {
-        int j = roads[cur_road].to;
-        if (j == p) continue;
-
-        long long cur1 = sz[i], cur2 = dp[i], cur3 = sz[j];
-        dp[j] = dp[i];
-        dp[j] -= sz[i];
-        dp[j] -= sz[j];
-        sz[i] -= sz[j];
-        sz[j] += sz[i];
-        dp[j] += sz[i];
-        dp[j] += sz[j];
-
-        res = max(res, dp[j]);
-
-        dfs2(i, j);
-
-        sz[j] = cur3;
-        sz[i] = cur1;
-        dp[i] = cur2;
-    }
-}
+int buf[4];
 
 
 int main() {
-    ios::sync_with_stdio(false);
 
 #ifdef local
     freopen("testdata.in", "r+", stdin);
     freopen("testdata.out", "w+", stdout);
 #endif // local
 
-    cin >> n;
-    for (int i = 1; i <= n - 1; i++) {
-        int u, v;
-        cin >> u >> v;
-        add(u, v);
-        add(v, u);
-    }
-    dfs1(-1, 1);
-    for (int i = 1; i <= n; i++) dp[1] += sz[i];
-    res = dp[1];
-    dfs2(-1, 1);
-    cout << res << endl;
+    //ios::sync_with_stdio(false);
+
+    string a,b,c;
+    cin>>a>>b>>c;
+
+    int a_val,b_val,c_val;
+
+    a_val=b_val=c_val=0;
+
+    if(a[1]=='s')   a_val+=100;
+    else if(a[1]=='m')  a_val+=200;
+    else if(a[1]=='p')  a_val+=300;
+
+    a_val+=a[0]-'0';
+
+    buf[1]=a_val;
+
+
+    if(b[1]=='s')   b_val+=100;
+    else if(b[1]=='m')  b_val+=200;
+    else if(b[1]=='p')  b_val+=300;
+
+    b_val+=b[0]-'0';
+
+    buf[2]=b_val;
+
+
+    if(c[1]=='s')   c_val+=100;
+    else if(c[1]=='m')  c_val+=200;
+    else if(c[1]=='p')  c_val+=300;
+
+    c_val+=c[0]-'0';
+
+    buf[3]=c_val;
+
+    sort(buf+1,buf+1+3);
+
+    if((buf[3]-buf[2]==1&&buf[2]-buf[1]==1&&buf[3]-buf[1]==2)
+       ||(buf[1]==buf[2]&&buf[2]==buf[3]))
+        cout<<0<<endl;
+    else if(buf[3]-buf[2]==1||
+            buf[2]-buf[1]==1||
+            buf[3]-buf[1]==2||
+            buf[2]-buf[1]==2||
+            buf[3]-buf[2]==2||
+            buf[2]==buf[1]
+            ||buf[2]==buf[3]
+            ||buf[1]==buf[3])
+        cout<<1<<endl;
+    else
+        cout<<2<<endl;
+
 }
