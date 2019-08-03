5b4661e78bb52a8b86ad23fc240b6cc57a5617c0
geniusjoe <843520313@qq.com>
2019-08-04 01:19:06 +0800

    Codeforces Round #574 (Div. 2) D2

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -6,61 +6,66 @@ using namespace std;
 
 const int MAXN = 100005;
 const int INF = 0x3f3f3f3f;
+const long long MOD = 998244353;
+long long n, res, mx_len;
+map<long long, long long> mp1, mp2;
 
-char tmp[20][20];
-char raw[MAXN];
-
-long long n, m, T, k;
-
-bool check(int l, int r) {
-    for (int i = 1; i <= n; i++) {
-        if (r - l + 1 < strlen(tmp[i]))
-            continue;
-        else {
-            int cur_len = strlen(tmp[i]);
-            bool flg = true;
-            for (int j = 0; j < cur_len; j++) {
-                if (tmp[i][cur_len - j - 1] == raw[r - j])
-                    continue;
-                else {
-                    flg = false;
-                    continue;
-                }
-            }
-            if (flg) return false;
-        }
+// a ^ n % m
+long long qpow(long long a, long long b, long long m) {
+    long long res = 1;
+    while (b) {
+        if (b & 1) res = res * a % m;
+        a = a * a % m, b >>= 1;
     }
-    return true;
+    return res;
+}
+
+void get_num(long long raw) {
+    long long ret = 0, i = 1;
+    while (raw) {
+        long long cur = raw % 10;
+        mp2[i] += cur;
+        i++;
+        raw /= 10;
+    }
+}
+
+long long get_len(long long raw) {
+    long long len = 0;
+    while (raw) {
+        raw /= 10;
+        len++;
+    }
+    mx_len = max(len, mx_len);
+    return len;
 }
 
 int main() {
 
 #ifdef local
-    freopen("testdata.in", "r+", stdin);
+    freopen("testdata.in", "r+", stdn);
     freopen("testdata.out", "w+", stdout);
 #endif // local
 
-    //ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
 
-    scanf("%s", raw);
-    int l = 0, r = 0, res_len = 0, res_pos = 0;
-    scanf("%I64d ", &n);
-    for (int i = 1; i <= n; i++)
-        scanf("%s", tmp[i]);
-    while (r < strlen(raw)) {
-        while (l <= r) {
-            if (check(l, r)) {
-                if (r - l + 1 > res_len) {
-                    res_len = r - l + 1;
-                    res_pos = l;
-                }
-                break;
-            }
-            l++;
-        }
-        r++;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        long long cur;
+        cin >> cur;
+        mp1[get_len(cur)]++;
+        get_num(cur);
     }
-    cout << res_len << " " << res_pos << endl;
+    long long cur_nm = 0;
+    for (int i = mx_len; i >= 1; i--) {
+        cur_nm += mp1[i];
+        res += qpow(10, 2 * (i - 1), MOD) * 11ll % MOD * cur_nm % MOD * mp2[i] % MOD;
+        for (int j = i - 1; j >= 1; j--) {
+            res += qpow(10, j + i - 1, MOD) * 2ll % MOD * mp1[j] % MOD * mp2[i] % MOD;
+        }
+        res %= MOD;
+    }
+    cout << res << endl;
 
     return 0;
 }
