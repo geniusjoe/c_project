8e70a2816622f4dbaa26b1822b70ee0ebf8a5c67
geniusjoe <843520313@qq.com>
2019-08-03 14:37:40 +0800

    Codeforces Beta Round #71 C

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -4,13 +4,34 @@
 #define debu
 using namespace std;
 
-const int MAXN = 500005;
+const int MAXN = 100005;
 const int INF = 0x3f3f3f3f;
 
-char s[105], t[105], p[105];
-int hsh1[26 + 5], hsh2[26 + 5];
+char tmp[20][20];
+char raw[MAXN];
 
 long long n, m, T, k;
+
+bool check(int l, int r) {
+    for (int i = 1; i <= n; i++) {
+        if (r - l + 1 < strlen(tmp[i]))
+            continue;
+        else {
+            int cur_len = strlen(tmp[i]);
+            bool flg = true;
+            for (int j = 0; j < cur_len; j++) {
+                if (tmp[i][cur_len - j - 1] == raw[r - j])
+                    continue;
+                else {
+                    flg = false;
+                    continue;
+                }
+            }
+            if (flg) return false;
+        }
+    }
+    return true;
+}
 
 int main() {
 
@@ -21,28 +42,25 @@ int main() {
 
     //ios::sync_with_stdio(false);
 
-    cin >> n >> m >> k;
-    if (n % 2 == 0) {
-        cout << "Marsel" << endl;
-        return 0;
-    }
-    bool flag = false;
-    for (int i = 1; i * i <= m; i++) {
-        if (m % i == 0) {
-            if (m / i >= k && i > 1) {
-                flag = true;
-                break;
-            } else if (i >= k && m / i > 1) {
-                flag = true;
+    scanf("%s", raw);
+    int l = 0, r = 0, res_len = 0, res_pos = 0;
+    scanf("%I64d ", &n);
+    for (int i = 1; i <= n; i++)
+        scanf("%s", tmp[i]);
+    while (r < strlen(raw)) {
+        while (l <= r) {
+            if (check(l, r)) {
+                if (r - l + 1 > res_len) {
+                    res_len = r - l + 1;
+                    res_pos = l;
+                }
                 break;
             }
+            l++;
         }
+        r++;
     }
-    if (flag)
-        cout << "Timur" << endl;
-    else
-        cout << "Marsel" << endl;
-
+    cout << res_len << " " << res_pos << endl;
 
     return 0;
 }
