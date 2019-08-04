3423fda404b66dac63f17e7b5f48f9bd7851b275
geniusjoe <843520313@qq.com>
2019-08-04 23:45:25 +0800

    Educational Codeforces Round 69 (Rated for Div. 2) B

--- demon/acm.py
+++ demon/acm.py
@@ -9,10 +9,7 @@ sys.stdout = open("testdata.in", "w")
 
 
 def main():
-    print(500000)
-
-    for i in range(500000):
-        print(random.randint(1,1e18))
+    
 
 
 if __name__ == "__main__":
--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -4,68 +4,51 @@
 #define debu
 using namespace std;
 
-const int MAXN = 100005;
+const int MAXN = 300005;
 const int INF = 0x3f3f3f3f;
+const long long LINF = 0x3f3f3f3f3f3f3f3f;
 const long long MOD = 998244353;
-long long n, res, mx_len;
-map<long long, long long> mp1, mp2;
 
-// a ^ n % m
-long long qpow(long long a, long long b, long long m) {
-    long long res = 1;
-    while (b) {
-        if (b & 1) res = res * a % m;
-        a = a * a % m, b >>= 1;
-    }
-    return res;
-}
-
-void get_num(long long raw) {
-    long long ret = 0, i = 1;
-    while (raw) {
-        long long cur = raw % 10;
-        mp2[i] += cur;
-        i++;
-        raw /= 10;
-    }
-}
-
-long long get_len(long long raw) {
-    long long len = 0;
-    while (raw) {
-        raw /= 10;
-        len++;
-    }
-    mx_len = max(len, mx_len);
-    return len;
-}
+long long n, T, m, k;
+long long buf[MAXN];
+long long dp[MAXN][15];
 
 int main() {
 
 #ifdef local
-    freopen("testdata.in", "r+", stdn);
+    freopen("testdata.in", "r+", stdin);
     freopen("testdata.out", "w+", stdout);
 #endif // local
 
+#ifdef local
+    auto start_time = clock();
+    cerr << setprecision(3) << fixed;
+#endif
+
     ios::sync_with_stdio(false);
 
-    cin >> n;
+    cin >> n >> m >> k;
+    for (int i = 1; i <= n; i++) cin >> buf[i];
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= m; j++)
+            dp[i][j] = -1e10;
+    long long res = 0;
     for (int i = 1; i <= n; i++) {
-        long long cur;
-        cin >> cur;
-        mp1[get_len(cur)]++;
-        get_num(cur);
-    }
-    long long cur_nm = 0;
-    for (int i = mx_len; i >= 1; i--) {
-        cur_nm += mp1[i];
-        res += qpow(10, 2 * (i - 1), MOD) * 11ll % MOD * cur_nm % MOD * mp2[i] % MOD;
-        for (int j = i - 1; j >= 1; j--) {
-            res += qpow(10, j + i - 1, MOD) * 2ll % MOD * mp1[j] % MOD * mp2[i] % MOD;
+        for (int j = 1; j <= m; j++) {
+            if (j == 1) dp[i][j] = max(dp[i - 1][m] + buf[i] - k, buf[i] - k);
+            else
+                dp[i][j] = dp[i - 1][j - 1] + buf[i];
+            res = max(res, dp[i][j]);
         }
-        res %= MOD;
     }
     cout << res << endl;
 
+
+#ifdef local
+    auto end_time = clock();
+    cerr << "Execution time: " << (end_time - start_time) * (int) 1e3 / CLOCKS_PER_SEC << " ms\n";
+#endif
+
     return 0;
+
 }
