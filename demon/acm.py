e087586174f9c8a9ee5d328461585560c68d265f
geniusjoe <843520313@qq.com>
2019-08-22 18:52:46 +0800

    Educational Codeforces Round 59 (Rated for Div. 2) D

--- demon/acm.py
+++ demon/acm.py
@@ -4,22 +4,8 @@ import sys
 
 # sys.stdout = open("testdata.in", "w")
 
-times = 1000000
-
-for m in range(1, 1000):
-    expt = 0
-    print(m)
-    for _ in range(times):
-        ary = []
-        gcd=0
-        while True:
-            j = random.randint(1, m)
-            ary.append(j)
-            if len(ary) == 1:
-                gcd = ary[0]
-            else:
-                gcd = math.gcd(gcd, j)
-            if gcd == 1:
-                break
-        expt += len(ary)
-    print(expt / times)
+tar=3
+rng=1000000
+buf=[0 for i in range(rng)]
+for i in range(rng):
+    if()
--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -2,15 +2,29 @@
 
 using namespace std;
 
-const long long MAXN = 5005;
+const long long MAXN = 5500;
 const long long INF = 0x3f3f3f3f;
 const long long LINF = 0x3f3f3f3f3f3f3f3f;
 const long long MOD = 1e9 + 7;
 const long long OVER_FLOW = 0xffffffff;
 
-long long n, q;
-vector<int> v[MAXN];
-int a[MAXN], b[MAXN][MAXN];
+long long n, q, m, k, T;
+string str;
+bool tmp[MAXN][MAXN];
+int buf[MAXN][MAXN];
+vector<int> v;
+
+bool ok(int x) {
+    for (long long i = x; i <= n; i += x) {
+        for (long long j = x; j <= n; j += x) {
+            long long u = buf[i][j] - buf[i - x][j] - buf[i][j - x] + buf[i - x][j - x];
+            if (u == x * x || u == 0)
+                continue;
+            else return false;
+        }
+    }
+    return true;
+}
 
 
 int main() {
@@ -31,6 +45,7 @@ int main() {
     2.实数精度
     4.做一些总比不做好.
     5.排序之前不能取模.
+    6.0-1子矩阵子序列:前缀和
 思考提醒：
     1.最大值和最小值问题可不可以用二分答案？
     2.有没有贪心策略？否则能不能dp？
@@ -38,24 +53,44 @@ int main() {
 */
 
     ios::sync_with_stdio(false);
-    cin >> n >> q;
-    for (long long i = 1; i <= q; i++) {
-        long long l, r;
-        cin >> l >> r;
-        for (long long j = l; j <= r; j++) v[j].push_back(i);
-    }
-    long long sm = 0;
+    cin >> n;
     for (long long i = 1; i <= n; i++) {
-        long long m = v[i].size();
-        if (m > 0) sm++;
-        if (m == 1) a[v[i][0]]++;
-        else if (m == 2) b[v[i][0]][v[i][1]]++;
-    }
-    long long res = 0;
-    for (long long i = 1; i <= q; i++) {
-        for (long long j = i + 1; j <= q; j++) {
-            res = max(res, sm - a[i] - a[j] - b[i][j]);
+        cin >> str;
+        for (long long j = 0; j < str.size(); j++) {
+            long long cur = 0;
+            if (str[j] >= '0' && str[j] <= '9') {
+                cur = str[j] - '0';
+            } else if (str[j] >= 'A' && str[j] <= 'F') {
+                cur = str[j] - 'A' + 10;
+            }
+            if (cur / 8 == 1) tmp[i][4 * j + 1] = true;
+            cur %= 8;
+            if (cur / 4 == 1) tmp[i][4 * j + 2] = true;
+            cur %= 4;
+            if (cur / 2 == 1) tmp[i][4 * j + 3] = true;
+            cur %= 2;
+            if (cur / 1 == 1) tmp[i][4 * j + 4] = true;
+            cur %= 1;
         }
+    }
+
+    for (long long i = 1; i <= n; i++) {
+        for (long long j = 1; j <= n; j++) {
+            buf[i][j] = buf[i - 1][j] + buf[i][j - 1] - buf[i - 1][j - 1] + tmp[i][j] ;
+        }
+    }
+
+    for (long long i = 1; i * i <= n; i++)
+        if (n % i == 0) {
+            v.push_back(i);
+            if (i != n / i) v.push_back(n / i);
+        }
+    sort(v.begin(), v.end());
+
+    int res = 0;
+    for (int i : v) {
+        if (ok(i)) res = i;
+        else continue;
     }
     cout << res << endl;
 
