3c625ec5844161b8fa79aaa6cdbb31239f5e6db5
geniusjoe <843520313@qq.com>
2019-08-25 19:43:30 +0800

    Educational Codeforces Round 53 (Rated for Div. 2) D

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -2,17 +2,16 @@
 
 using namespace std;
 
-const long long MAXN = 100500;
+const long long MAXN = 300500;
 const long long INF = 0x3f3f3f3f;
 const long long LINF = 0x3f3f3f3f3f3f3f3f;
 const long long MOD = 998244353;
 const long long OVER_FLOW = 0xffffffff;
 
-long long n, q, m, k, T;
+long long n, m, k, T;
 long long buf[MAXN];
-vector<pair<long long, long long>> dgre_else;
-vector<long long> dgre_1;
-vector<pair<long long, long long>> res;
+map<int, int> nxt, lst;
+
 
 int main() {
 
@@ -40,49 +39,40 @@ int main() {
 */
 
     ios::sync_with_stdio(false);
-    cin >> n;
+    cin >> n >> T;
     long long sm = 0;
+    nxt[0] = 1;
+    lst[1] = 0;
     for (long long i = 1; i <= n; i++) {
-        long long u;
-        cin >> u;
-        if (u == 1) dgre_1.push_back(i);
-        else dgre_else.emplace_back(i, u);
-        sm += u;
+        cin >> buf[i];
+        sm += buf[i];
+        if (i == n) nxt[i] = -1;
+        else nxt[i] = i + 1;
+        lst[i] = i - 1;
     }
-    long long len = 0;
-    if (sm < 2 * n - 2) cout << "NO" << endl;
-    else {
-        len = dgre_else.size() - 1;
-        for (long long i = 1; i < dgre_else.size(); i++) {
-            res.emplace_back(dgre_else[i - 1].first, dgre_else[i].first);
-            dgre_else[i - 1].second--;
-            dgre_else[i].second--;
-        }
-        if (!dgre_1.empty()) {
-            len++;
-            res.emplace_back(dgre_1.back(), dgre_else[0].first);
-            dgre_1.pop_back();
-            dgre_else[0].second--;
-        }
-        if (!dgre_1.empty()) {
-            len++;
-            res.emplace_back(dgre_1.back(), dgre_else.back().first);
-            dgre_1.pop_back();
-            dgre_else.back().second--;
-        }
-        while (!dgre_1.empty()) {
-            while (dgre_else.back().second == 0) dgre_else.pop_back();
-            res.emplace_back(dgre_1.back(), dgre_else.back().first);
-            dgre_else.back().second--;
-            dgre_1.pop_back();
-        }
+    long long res = 0, nm = n;
+    long long mn = *min_element(buf + 1, buf + 1 + n);
 
-        cout << "YES" << " " << len << endl;
-        cout << res.size() << endl;
-        for (auto it:res) {
-            cout << it.first << " " << it.second << endl;
+    while (T >= mn) {
+        if (T >= sm) {
+            long long tme = T / sm;
+            T -= T / sm * sm;
+            res += nm * tme;
+        } else {
+            for (long long i = nxt[0]; i != -1; i = nxt[i]) {
+                if (T >= buf[i]) {
+                    T -= buf[i];
+                    res++;
+                } else if (T < buf[i]) {
+                    nxt[lst[i]] = nxt[i];
+                    lst[nxt[i]] = lst[i];
+                    sm -= buf[i];
+                    nm--;
+                }
+            }
         }
     }
+    cout << res << endl;
 
 
 #ifndef ONLINE_JUDGE
