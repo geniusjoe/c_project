86b47d9bed3e479c276725725afdf09e54e58e7f
geniusjoe <843520313@qq.com>
2019-09-11 12:44:50 +0800

    1204E - Natasha, Sasha and the Prefix Sums

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -2,28 +2,22 @@
 
 using namespace std;
 
-const long long MAXN = 200500;
+const long long MAXN = 2005;
 const long long INF = 0x7f3f3f3f;
 const long long LINF = 0x1f3f3f3f3f3f3f3f;
-const long long MOD = (long long) 1e9 + 7;
+const long long MOD = 998244853;
 const long long OVER_FLOW = 0xffffffff;
 
 long long n, m;
 
-#define setbit(x, y) x|=(1ll<<y)
-
-#define clrbit(x, y) x&=~(1ll<<y)
-
-#define reversebit(x, y) x^=(1ll<<y)
-
-#define getbit(x, y) ((x) >> (y)&1ll)
+long long k[MAXN][MAXN], f[MAXN][MAXN], C[MAXN << 1][MAXN << 1];
 
 
 int main() {
 
 #ifndef ONLINE_JUDGE
-//    freopen("testdata.in", "r+", stdin);
-//    freopen("testdata.out", "w+", stdout);
+    freopen("testdata.in", "r+", stdin);
+    freopen("testdata.out", "w+", stdout);
 #endif // ONLINE_JUDGE
 
 #ifndef ONLINE_JUDGE
@@ -48,34 +42,35 @@ int main() {
 */
 
     ios::sync_with_stdio(false);
+    cin >> n >> m;
 
-    cout << "? ";
-    for (long long i = 1; i <= 100; i++) {
-        cout << i << " ";
+    for (long long i = 0; i <= n; i++) {
+        for (long long j = 0; j <= m; j++) {
+            if (i == 0) k[i][j] = 1;
+            else if (j == 0) k[i][j] = 0;
+            else if (i > j) k[i][j] = 0;
+            else k[i][j] = (k[i - 1][j] + k[i][j - 1]) % MOD;
+        }
     }
-    cout << endl;
-    fflush(stdout);
-    long long res = 0;
-    cin >> res;
-    for (long long i = 0; i < 7; i++) {
-        clrbit(res, i);
-    }
-    cout << endl;
-    fflush(stdout);
 
-    cout << "? ";
-    for (long long i = 1; i <= 100; i++) {
-        cout << (i << 7) << " ";
+    C[0][0] = C[1][0] = C[1][1] = 1;
+    for (int i = 2; i <= n + m; ++i) {
+        C[i][0] = 1;
+        for (int j = 1; j <= i; ++j)
+            C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % MOD;
     }
-    cout << endl;
-    fflush(stdout);
-    long long res1 = 0;
-    cin >> res1;
-    for (long long i = 7; i < 14; i++) {
-        clrbit(res1, i);
-    }
-    cout << "! " << (long long) (res1 | res) << endl;
 
+    for (long long i = 0; i <= n; i++) {
+        f[i][0] = i, f[0][i] = 0;
+    }
+    for (long long i = 1; i <= n; i++) {
+        for (long long j = 1; j <= m; j++) {
+            f[i][j] = ((f[i - 1][j] + C[i + j - 1][i - 1]) % MOD +
+                       (f[i][j - 1] - C[i + j - 1][j - 1] + k[i][j - 1] + MOD)) %
+                      MOD;
+        }
+    }
+    cout << f[n][m] << endl;
 
 #ifndef ONLINE_JUDGE
     auto end_time = clock();
