6cf37d7c17236297400cdf44c04f0fe0f31a9970
geniusjoe <843520313@qq.com>
2019-09-11 17:09:57 +0800

    1204D2 - Kirk and a Binary String (hard version)

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -42,35 +42,15 @@ int main() {
 */
 
     ios::sync_with_stdio(false);
-    cin >> n >> m;
-
-    for (long long i = 0; i <= n; i++) {
-        for (long long j = 0; j <= m; j++) {
-            if (i == 0) k[i][j] = 1;
-            else if (j == 0) k[i][j] = 0;
-            else if (i > j) k[i][j] = 0;
-            else k[i][j] = (k[i - 1][j] + k[i][j - 1]) % MOD;
-        }
+    string str;
+    cin >> str;
+    long long cnt = 0;
+    for (long long i = str.size() - 1; i >= 0; i--) {
+        if (str[i] == '0') cnt++;
+        else if (cnt) cnt--;
+        else str[i] = '0';
     }
-
-    C[0][0] = C[1][0] = C[1][1] = 1;
-    for (int i = 2; i <= n + m; ++i) {
-        C[i][0] = 1;
-        for (int j = 1; j <= i; ++j)
-            C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % MOD;
-    }
-
-    for (long long i = 0; i <= n; i++) {
-        f[i][0] = i, f[0][i] = 0;
-    }
-    for (long long i = 1; i <= n; i++) {
-        for (long long j = 1; j <= m; j++) {
-            f[i][j] = ((f[i - 1][j] + C[i + j - 1][i - 1]) % MOD +
-                       (f[i][j - 1] - C[i + j - 1][j - 1] + k[i][j - 1] + MOD)) %
-                      MOD;
-        }
-    }
-    cout << f[n][m] << endl;
+    cout << str << endl;
 
 #ifndef ONLINE_JUDGE
     auto end_time = clock();
