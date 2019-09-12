8179fc9dd836acd5011352734632c34c6346b547
geniusjoe <843520313@qq.com>
2019-09-12 12:40:54 +0800

    Manthan, Codefest 19 (open for everyone, rated, Div. 1 + Div. 2) E

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -2,94 +2,16 @@
 
 using namespace std;
 
-const long long MAXN = 200050;
+const long long MAXN = 1000500;
 const long long INF = 0x7f3f3f3f;
 const long long LINF = 0x1f3f3f3f3f3f3f3f;
 const long long MOD = 998244853;
 const long long OVER_FLOW = 0xffffffff;
 
 long long n, m; /**< n个数,m个操作,以model为模 */
-long long a[MAXN], ans[MAXN << 2];
-long long add[MAXN << 2]; /**< add 代表加的add,mul代表乘的add */
-
 long long res[MAXN];
-
-inline long long lchild(long long x)
-/**<  每个树节点的编号*/
-{
-    return x << 1;
-}
-
-inline long long rchild(long long x) {
-    return x << 1 | 1;
-}
-
-inline void push_up(long long p) {
-    ans[p] = min(ans[lchild(p)], ans[rchild(p)]);
-}
-
-/**< 由顶向下建立线段树 */
-/**< 左右均为闭区间 */
-void build(long long p, long long l, long long r) {
-    add[p] = 0;
-    if (l == r) {
-        ans[p] = a[l];
-        add[p] = 0;
-        return;
-    }
-    long long mid = (l + r) >> 1;
-    build(lchild(p), l, mid);
-    build(rchild(p), mid + 1, r);
-    push_up(p);
-}
-
-inline void f(long long p, long long l, long long r,
-              long long cur_add) {
-    add[p] += cur_add;
-    ans[p] += cur_add;
-}
-
-inline void push_down(long long p, long long l, long long r) {
-    long long mid = (l + r) >> 1;
-    f(lchild(p), l, mid, add[p]);
-    f(rchild(p), mid + 1, r, add[p]);
-    add[p] = 0;
-}
-/**< 区间赋值操作 */
-/**< 区间内元素增加的值 */
-inline void update(long long nl, long long nr,   /**< 目标边界 */
-                   long long l, long long r, long long p, long long add_num /**< 当前边界和节点 */
-) {
-    if (nl <= l && r <= nr) {
-        ans[p] += add_num;
-        add[p] += add_num;
-        return;
-    }
-    push_down(p, l, r);
-    long long mid = (l + r) >> 1;
-    if (nl <= mid)
-        update(nl, nr, l, mid, lchild(p), add_num);
-    if (nr > mid)
-        update(nl, nr, mid + 1, r, rchild(p), add_num);
-    push_up(p);
-}
-
-/**< 区间查询操作 */
-long long query(long long l, long long r, long long p) {
-    long long res = 0;
-    if (l == r) {
-        ans[p] = LINF;
-        return l;
-    }
-    long long mid = (l + r) >> 1;
-    push_down(p, l, r);
-    if (ans[p] == ans[rchild(p)])
-        res = query(mid + 1, r, rchild(p));
-    else
-        res = query(l, mid, lchild(p));
-    push_up(p);
-    return res;
-}
+vector<pair<long long, long long>> in[MAXN], out[MAXN];
+multiset<long long> st[MAXN];
 
 
 int main() {
@@ -121,21 +43,49 @@ int main() {
 */
 
     ios::sync_with_stdio(false);
-    cin >> n;
-    for (long long i = 1; i <= n; i++) cin >> a[i];
-    build(1, 1, n);
+    long long w;
+    cin >> n >> w;
     for (long long i = 1; i <= n; i++) {
-        long long cur = query(1, n, 1);
-        res[cur] = i;
-        if (cur < n) {
-            update(cur + 1, n, 1, n, 1, -i);
+        long long len;
+        cin >> len;
+        long long l = 1, r = w - len + 1;
+        in[l].emplace_back(0, i);
+        out[r].emplace_back(0, i);
+
+        l = len + 1, r = w + 1;
+        in[l].emplace_back(0, i);
+        out[r].emplace_back(0, i);
+
+        for (long long j = 1; j <= len; j++) {
+            long long u;
+            cin >> u;
+            l = j, r = j + (w - len) + 1;
+            in[l].emplace_back(u, i);
+            out[r].emplace_back(u, i);
         }
     }
-    for (long long i = 1; i <= n; i++) {
+
+    for (long long i = 1; i <= w; i++) {
+        res[i] = res[i - 1];
+        for (auto it:in[i]) {
+            if (!st[it.second].empty())
+                res[i] -= *st[it.second].rbegin();
+            st[it.second].insert(it.first);
+            res[i] += *st[it.second].rbegin();
+        }
+
+        for (auto it:out[i]) {
+            if (!st[it.second].empty())
+                res[i] -= *st[it.second].rbegin();
+            st[it.second].erase(st[it.second].find(it.first));
+            res[i] += *st[it.second].rbegin();
+        }
+    }
+
+    for (long long i = 1; i <= w; i++) {
         cout << res[i] << " ";
     }
     cout << endl;
-
 
 #ifndef ONLINE_JUDGE
     auto end_time = clock();
