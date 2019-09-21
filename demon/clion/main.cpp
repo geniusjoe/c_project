ae5059c7277180eaa2b3ac9693901ca5e165baa4
geniusjoe <843520313@qq.com>
2019-09-21 20:09:41 +0800

    Educational Codeforces Round 67 (Rated for Div. 2) D

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -2,14 +2,75 @@
 
 using namespace std;
 
-const long long MAXN = 1050;
+const long long MAXN = 300500;
 const long long INF = 0x3f3f3f3f;
 const long long LINF = 0x1f3f3f3f3f3f3f3f;
 const long long MOD = (long long) 998244353;
 const long long OVER_FLOW = 0xffffffff;
 
 long long n;
-long long a[MAXN], f[MAXN][MAXN], res[100050], s[MAXN][MAXN];
+long long buf[MAXN], tar[MAXN];
+queue<long long> idx[MAXN];
+
+long long ans[MAXN << 2];
+
+inline long long lchild(long long x)
+/**<  每个树节点的编号*/
+{
+    return x << 1;
+}
+
+inline long long rchild(long long x) {
+    return x << 1 | 1;
+}
+
+inline void push_up(long long p) {
+    ans[p] = min(ans[lchild(p)], ans[rchild(p)]);
+}
+
+/**< 由顶向下建立线段树 */
+/**< 左右均为闭区间 */
+void build(long long p, long long l, long long r) {
+    if (l == r) {
+        ans[p] = tar[l];
+        return;
+    }
+    long long mid = (l + r) >> 1;
+    build(lchild(p), l, mid);
+    build(rchild(p), mid + 1, r);
+    push_up(p);
+}
+/**< 区间赋值操作 */
+/**< 区间内元素增加的值 */
+inline void update(long long nl, long long nr,   /**< 目标边界 */
+                   long long l, long long r, long long p /**< 当前边界和节点 */
+) {
+    if (nl <= l && r <= nr) {
+        ans[p] = INF;
+        return;
+    }
+    long long mid = (l + r) >> 1;
+    if (nl <= mid)
+        update(nl, nr, l, mid, lchild(p));
+    if (nr > mid)
+        update(nl, nr, mid + 1, r, rchild(p));
+    push_up(p);
+}
+
+/**< 区间查询操作 */
+long long query(long long q_x, long long q_y, long long l, long long r, long long p) {
+    long long res = INF;
+    if (q_x <= l && r <= q_y) {
+        return ans[p];
+    }
+    long long mid = (l + r) >> 1;
+    if (q_x <= mid) {
+        res = min(res, query(q_x, q_y, l, mid, lchild(p)));
+    }
+    if (q_y > mid)
+        res = min(res, query(q_x, q_y, mid + 1, r, rchild(p)));
+    return res;
+}
 
 int main() {
 
@@ -41,27 +102,44 @@ int main() {
 */
 
     ios::sync_with_stdio(false);
-    long long k;
-    cin >> n >> k;
-    for (long long i = 1; i <= n; i++) cin >> a[i];
-    sort(a + 1, a + 1 + n);
-    a[0] = -INF;
-    long long ans = 0;
-    for (long long v = 1; v * (k - 1) <= a[n]; v++) {
-        f[0][0] = 1;
-        long long now = 0;
-        s[0][0] = 1;
+    long long T;
+    cin >> T;
+    while (T--) {
+        for (long long i = 1; i <= n; i++) while (!idx[i].empty()) idx[i].pop();
+        cin >> n;
+
         for (long long i = 1; i <= n; i++) {
-            while (a[now] <= a[i] - v) now++;
-            for (long long j = 0; j <= k; j++) {
-                if (j) f[i][j] = s[now - 1][j - 1];
-                s[i][j] = (s[i - 1][j] + f[i][j]) % MOD;
-            }
-            res[v] += f[i][k];
+            long long u;
+            cin >> u;
+            idx[u].push(i);
+            tar[i] = u;
         }
-        ans += res[v] % MOD;
+        for (long long i = 1; i <= n; i++) {
+            cin >> buf[i];
+        }
+
+        build(1, 1, n);
+        bool flg = true;
+        for (long long i = 1; i <= n; i++) {
+            if (idx[buf[i]].empty()) {
+                flg = false;
+                break;
+            } else {
+                long long pos = idx[buf[i]].front(), cur = i;
+                idx[buf[i]].pop();
+                if (buf[cur] != query(1, pos, 1, n, 1)) {
+                    flg = false;
+                    break;
+                }
+                update(pos, pos, 1, n, 1);
+            }
+        }
+
+        if (flg) {
+            cout << "YES" << endl;
+        } else
+            cout << "NO" << endl;
     }
-    cout << ans % MOD << endl;
 
 
 #ifndef ONLINE_JUDGE
