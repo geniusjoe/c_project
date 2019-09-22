31f6161f344baa74b5fe7b739336d302f7b990ac
geniusjoe <843520313@qq.com>
2019-09-22 13:55:55 +0800

    Codeforces Round #568 (Div. 2) E

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -2,92 +2,16 @@
 
 using namespace std;
 
-const long long MAXN = 1005000;
+const long long MAXN = 2050;
 const long long INF = 0x3f3f3f3f;
 const long long LINF = 0x1f3f3f3f3f3f3f3f;
 const long long MOD = (long long) 998244353;
 const long long OVER_FLOW = 0xffffffff;
 
 long long n, m, q;
-long long price[MAXN], money[MAXN];
-
-
-long long ans[MAXN << 2];
-long long add[MAXN << 2]; /**< add 代表加的add,mul代表乘的add */
-long long add_num; /**< 每个操作的操作数 */
-inline long long lchild(long long x)
-/**<  每个树节点的编号*/
-{
-    return x << 1;
-}
-
-inline long long rchild(long long x) {
-    return x << 1 | 1;
-}
-
-inline void push_up(long long p) {
-    ans[p] = max(ans[lchild(p)], ans[rchild(p)]);
-}
-
-/**< 由顶向下建立线段树 */
-/**< 左右均为闭区间 */
-void build(long long p, long long l, long long r) {
-    add[p] = 0;
-    if (l == r) {
-        ans[p] = 0;
-        return;
-    }
-    long long mid = (l + r) >> 1;
-    build(lchild(p), l, mid);
-    build(rchild(p), mid + 1, r);
-    push_up(p);
-}
-
-inline void f(long long p, long long l, long long r,
-              long long cur_add) {
-    add[p] = add[p] + cur_add;
-    ans[p] = ans[p] + cur_add;
-}
-
-inline void push_down(long long p, long long l, long long r) {
-    long long mid = (l + r) >> 1;
-    f(lchild(p), l, mid, add[p]);
-    f(rchild(p), mid + 1, r, add[p]);
-    add[p] = 0;
-}
-/**< 区间赋值操作 */
-/**< 区间内元素增加的值 */
-inline void update(long long nl, long long nr,   /**< 目标边界 */
-                   long long l, long long r, long long p /**< 当前边界和节点 */
-) {
-    if (nl <= l && r <= nr) {
-        ans[p] = (ans[p] + add_num);
-        add[p] = (add[p] + add_num);
-        return;
-    }
-    push_down(p, l, r);
-    long long mid = (l + r) >> 1;
-    if (nl <= mid)
-        update(nl, nr, l, mid, lchild(p));
-    if (nr > mid)
-        update(nl, nr, mid + 1, r, rchild(p));
-    push_up(p);
-}
-
-/**< 区间查询操作 */
-long long query(long long l, long long r, long long p) {
-    long long res = 0;
-    if (l == r)
-        if (ans[p] > 0)
-            return l;
-        else return -1;
-    long long mid = (l + r) >> 1;
-    push_down(p, l, r);
-    if (ans[rchild(p)] > 0)
-        res = query(mid + 1, r, rchild(p));
-    else res = query(l, mid, lchild(p));
-    return res;
-}
+string buf[MAXN];
+char tar[MAXN][MAXN];
+vector<pair<long long, long long >> num[MAXN];
 
 int main() {
 
@@ -119,38 +43,83 @@ int main() {
 */
 
     ios::sync_with_stdio(false);
-    cin >> n >> m;
-    const long long r = 1e6 + 5;
-    build(1, 1, r);
+    long long T;
+    cin >> T;
+    while (T--) {
+        cin >> n >> m;
+        for (long long i = 1; i <= n; i++) cin >> buf[i];
+        for (long long i = 1; i <= n; i++) for (long long j = 1; j <= m; j++) tar[i][j - 1] = '.';
+        for (long long i = 0; i <= 26; i++) num[i].clear();
 
-    for (long long i = 1; i <= n; i++) {
-        cin >> price[i];
-        add_num = 1;
-        update(1, price[i], 1, r, 1);
-    }
-    for (long long i = 1; i <= m; i++) {
-        cin >> money[i];
-        add_num = -1;
-        update(1, money[i], 1, r, 1);
-    }
-
-    cin >> q;
-
-    for (long long i = 1; i <= q; i++) {
-        long long u, v, x;
-        cin >> u >> v >> x;
-        if (u == 1) {
-            add_num = -1;
-            update(1, price[v], 1, r, 1);
-            add_num = 1, price[v] = x;
-            update(1, price[v], 1, r, 1);
-        } else {
-            add_num = 1;
-            update(1, money[v], 1, r, 1);
-            add_num = -1, money[v] = x;
-            update(1, money[v], 1, r, 1);
+        bool flg = true;
+        for (long long i = 1; i <= n; i++) {
+            for (long long j = 1; j <= m; j++) {
+                if (buf[i][j - 1] == '.') continue;
+                else {
+                    long long cur = buf[i][j - 1] - 'a';
+                    if (num[cur].empty() or num[cur].size() == 1) num[cur].emplace_back(i, j);
+                    else if ((num[cur][0].first == num[cur][1].first and num[cur][0].first == i) or
+                             (num[cur][0].second == num[cur][1].second and num[cur][0].second == j)) {
+                        num[cur].pop_back();
+                        num[cur].emplace_back(i, j);
+                    } else {
+                        flg = false;
+                        break;
+                    }
+                }
+            }
+            if (!flg) break;
         }
-        cout << query(1, r, 1) << '\n';
+        long long lst = -1;
+        for (long long i = 0; i <= 26; i++) {
+            if (num[i].empty()) continue;
+            else {
+                lst = i;
+                if (num[i].size() == 1) {
+                    auto cur = num[i].back();
+                    tar[cur.first][cur.second - 1] = i + 'a';
+                } else if (num[i].size() == 2) {
+                    auto front = num[i][0], tail = num[i][1];
+                    if (front.first == tail.first) {
+                        for (long long j = front.second; j <= tail.second; j++)
+                            tar[front.first][j - 1] = i + 'a';
+                    } else if (front.second == tail.second) {
+                        for (long long j = front.first; j <= tail.first; j++)
+                            tar[j][front.second - 1] = i + 'a';
+                    }
+                }
+            }
+        }
+
+        for (long long i = 1; i <= n; i++) {
+            for (long long j = 1; j <= m; j++) {
+                if (buf[i][j - 1] == tar[i][j - 1]) continue;
+                else flg = false;
+            }
+            if (!flg) break;
+        }
+
+        if (!flg)
+            cout << "NO" << '\n';
+        else {
+            cout << "YES" << '\n';
+            if (lst == -1) cout << "0" << '\n';
+            else {
+                cout << lst + 1 << '\n';
+                for (long long i = 0; i <= lst; i++) {
+                    if (num[i].empty()) {
+                        auto cur = num[lst].back();
+                        cout << cur.first << " " << cur.second << " " << cur.first << " " << cur.second << "\n";
+                    } else if (num[i].size() == 1) {
+                        auto cur = num[i].back();
+                        cout << cur.first << " " << cur.second << " " << cur.first << " " << cur.second << "\n";
+                    } else if (num[i].size() == 2) {
+                        auto cur1 = num[i][0], cur2 = num[i][1];
+                        cout << cur1.first << " " << cur1.second << " " << cur2.first << " " << cur2.second << "\n";
+                    }
+                }
+            }
+        }
     }
 
 
