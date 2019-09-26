d223bea93ba5dd9df554bb3494d9c0a4dcf10794
geniusjoe <843520313@qq.com>
2019-09-26 23:36:32 +0800

    Codeforces Round #566 (Div. 2) E

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -3,31 +3,60 @@
 using namespace std;
 
 const long long MAXN = 200500;
+const long long PHI = (long long) 1e9 + 6;
 const long long INF = 0x3f3f3f3f;
 const long long LINF = 0x1f3f3f3f3f3f3f3f;
-const long long MOD = (long long) 998244353;
+const long long MOD = (long long) 1e9 + 7;
 const long long OVER_FLOW = 0xffffffff;
 
 long long n, m, q;
 long long buf[MAXN];
 
-template<class T>
-inline bool scan_d(T &ret) {
-    char c;
-    int sgn;
-    if (c = getchar(), c == EOF) return 0;
-    while (c != '-' && (c < '0' || c > '9')) c = getchar();
-    sgn = (c == '-') ? -1 : 1;
-    ret = (c == '-') ? 0 : (c - '0');
-    while (c = getchar(), c >= '0' && c <= '9') ret = ret * 10 + (c - '0');
-    ret *= sgn;
-    return 1;
+struct Matrix {
+    long long mat[7][7];
+    long long row, col;
+
+    Matrix(long long x, long long y) {
+        memset(this->mat, 0, sizeof this->mat);
+        this->row = x, this->col = y;
+    }
+};
+
+Matrix mul_M(Matrix a, Matrix b, long long mod) {
+    Matrix ret(a.row, b.col);
+    for (int i = 0; i < a.row; i++)
+        for (int j = 0; j < a.col; j++) {
+            ret.mat[i][j] = 0;
+            for (int k = 0; k < b.row; k++) {
+                ret.mat[i][j] += a.mat[i][k] * b.mat[k][j] % mod;
+                ret.mat[i][j] %= mod;
+            }
+        }
+    return ret;
 }
 
-inline void out(int x) {
-    if (x > 9) out(x / 10);
-    putchar(x % 10 + '0');
+Matrix pow_M(Matrix a, long long n, long long mod) {
+    Matrix ret(a.row, a.col);
+    for (int i = 0; i < ret.col; i++)ret.mat[i][i] = 1;
+    Matrix tmp = a;
+    while (n) {
+        if (n & 1)ret = mul_M(ret, tmp, mod);
+        tmp = mul_M(tmp, tmp, mod);
+        n >>= 1;
+    }
+    return ret;
 }
+
+// a ^ b % c
+long long qpow(long long a, long long b, long long c) {
+    long long cur = 1;
+    while (b) {
+        if (b & 1) cur = cur * a % c;
+        a = a * a % c, b >>= 1;
+    }
+    return cur;
+}
+
 
 int main() {
 
@@ -59,38 +88,29 @@ int main() {
 */
 
 //    ios::sync_with_stdio(false);
-    long long T;
-    cin >> T;
-    while (T--) {
-        cin >> n;
-        long long res = 0;
-        for (long long i = 1; i <= n; i++)
-            scan_d(buf[i]);
-        res = max(res, *max_element(buf + 1, buf + 1 + n));
-        long long flg2 = false, flg3 = false, flg5 = false;
-        for (long long i = 1; i <= n; i++) {
-            if (res == 2 * buf[i]) flg2 = true;
-            if (res == 3 * buf[i]) flg3 = true;
-            if (res == 5 * buf[i]) flg5 = true;
-        }
-        if (flg2 and flg3 and flg5) res = max(res, res / 2 + res / 3 + res / 5);
-        sort(buf + 1, buf + 1 + n);
-        for (long long i = 1; i < n; i++) {
-            if (buf[n] % buf[i] == 0) {
-                buf[i] = INF;
-            }
-        }
-        sort(buf + 1, buf + 1 + n);
-        n = lower_bound(buf + 1, buf + 1 + n, INF) - buf - 1;
-        if (n >= 2) res = max(res, buf[n] + buf[n - 1]);
-        for (long long i = n - 2; i >= 1; i--) {
-            if (buf[n - 1] % buf[i] != 0)
-                res = max(res, buf[n] + buf[n - 1] + buf[i]);
-        }
-        cout << res << '\n';
+    long long f1, f2, f3, w;
+    cin >> n >> f1 >> f2 >> f3 >> w;
+    Matrix a(3, 3), b(5, 5), a1(3, 1), b1(3, 1), c1(3, 1), d1(5, 1);
+    a.mat[0][0] = a.mat[0][1] = a.mat[0][2] = a.mat[1][0] = a.mat[2][1] = 1;
 
-    }
+    a1.mat[2][0] = 1, b1.mat[1][0] = 1, c1.mat[0][0] = 1;
+    a = pow_M(a, n - 3, PHI);
+    a1 = mul_M(a, a1, PHI), b1 = mul_M(a, b1, PHI), c1 = mul_M(a, c1, PHI);
 
+    b.mat[0][0] = b.mat[0][1] = b.mat[0][2] = b.mat[0][3] = b.mat[0][4] = 1;
+    b.mat[1][0] = b.mat[2][1] = b.mat[3][3] = b.mat[3][4] = b.mat[4][4] = 1;
+    b = pow_M(b, n - 3, PHI);
+//    d1.mat[0][0] = f3, d1.mat[1][0] = f2, d1.mat[2][0] = f1, d1.mat[4][0] = 2;
+    d1.mat[4][0] = 2;
+    d1 = mul_M(b, d1, PHI);
+
+    long long res = 0;
+    res = qpow(w, d1.mat[0][0], MOD)
+          * qpow(f1, a1.mat[0][0], MOD) % MOD
+          * qpow(f2, b1.mat[0][0], MOD) % MOD
+          * qpow(f3, c1.mat[0][0], MOD) % MOD;
+
+    cout << res << endl;
 
 #ifndef ONLINE_JUDGE
     auto end_time = clock();
