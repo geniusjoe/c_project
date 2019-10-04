38863a956c67c0b853dab0f9f6f7825ef4a4d6f7
geniusjoe <843520313@qq.com>
2019-10-05 01:42:13 +0800

    Good Bye 2018 D

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-const long long MAXN = 200500;
+const long long MAXN = 3005000;
 const long long PHI = (long long) 998244352;
 const long long INF = 0x3f3f3f3f;
 const long long LINF = 0x1f3f3f3f3f3f3f3f;
@@ -11,26 +11,7 @@ const long long OVER_FLOW = 0xffffffff;
 
 long long n, K, m;
 
-const long long MAX_BASE = 30;
-long long a[MAXN], b[MAXN]; // a为所有元素的集合,b为线性基
-long long cal() {
-    long long res = 0;
-    for (long long i = 1; i <= n; ++i) {
-        for (long long j = MAX_BASE; j >= 0; --j) {
-            if (a[i] >> j & 1) {
-                if (b[j]) a[i] ^= b[j];
-                else {
-                    b[j] = a[i];
-                    for (long long k = j - 1; k >= 0; --k) if (b[k] && (b[j] >> k & 1)) b[j] ^= b[k];
-                    for (long long k = j + 1; k <= MAX_BASE; ++k) if (b[k] >> j & 1) b[k] ^= b[j];
-                    res++;
-                    break;
-                }
-            }
-        }
-    }
-    return res;
-}
+long long fac[MAXN];
 
 
 int main() {
@@ -48,29 +29,30 @@ int main() {
 /*
 写代码时请注意：
     1.数学公式尝试化简
-    2.数量值太大尝试给出递推公式
+    2.数量值太大尝试给出矩阵递推公式
     5.排序之前不能取模.
     6.0-1子矩阵子序列:前缀和,异或和
     7.模拟题注意代码复用.
     8.单个区间问题考虑前缀和,多个区间左右端点拆开,排序,记录端点极值
 思考提醒：
     1.最大值和最小值问题可不可以用二分+贪心？
-    2.dp
+    2.dp,考虑递推
     3.如果规模小的话还能尝试dfs
     4.如果存在等式.转换关系少可以暴力枚举变量,或者考虑从数据大小入手
     5.离散化+打表
 */
 
     ios::sync_with_stdio(false);
-    cin>>n;
-    for(long long i=1;i<=n;i++) cin>>a[i];
-    long long cur=0;
-    for(long long i=1;i<=n;i++) cur^=a[i];
-    if(cur==0){
-        cout<<-1<<endl;
-    }else{
-        cout<<cal()<<endl;
+    cin >> n;
+    long long cur = 1;
+
+    fac[0] = 1;
+    for (long long i = 1; i <= 1005000; i++) fac[i] = i * fac[i - 1] % MOD;
+
+    for (long long i = 2; i <= n; i++) {
+        cur = (MOD + cur + fac[i - 1] % MOD - 1) % MOD * i % MOD;
     }
+    cout << cur << endl;
 
 
 #ifndef ONLINE_JUDGE
