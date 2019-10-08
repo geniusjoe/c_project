1c1d62fc34b59e99f0fab86b0d3e90a9fd0990f0
geniusjoe <843520313@qq.com>
2019-10-08 21:29:13 +0800

    Avito Cool Challenge 2018 C

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -1,8 +1,10 @@
 #include<bits/stdc++.h>
 
+#pragma GCC optimize(2)
+
 using namespace std;
 
-const long long MAXN = 200500;
+const long long MAXN = 2050;
 const long long PHI = (long long) 998244352;
 const long long INF = 0x3f3f3f3f;
 const long long LINF = 0x1f3f3f3f3f3f3f3f;
@@ -10,7 +12,7 @@ const long long MOD = (long long) 998244353;
 const long long OVER_FLOW = 0xffffffff;
 
 long long n;
-vector<pair<long long, long long>> buf[MAXN];
+long long dp[MAXN][MAXN];
 
 int main() {
 
@@ -27,7 +29,7 @@ int main() {
 /*
 写代码时请注意：
     1.数学公式尝试化简
-    2.dp或者数值太大,尝试给出递推公式或者预处理数据
+    2.dp或者数值太大,尝试给出递推公式
     3.概率题随机生成考虑1/2的情况,或者是满足的方案/所有可能的方案
     5.排序之前不能取模.
     6.0-1子矩阵子序列:前缀和,异或和
@@ -42,41 +44,17 @@ int main() {
 */
 
     ios::sync_with_stdio(false);
-    const long long m = 2e5 + 5;
-    for (long long i = 1; i <= m; i++) {
-        for (long long j = i + 1; j * j - i * i <= m; j++) {
-            buf[j * j - i * i].emplace_back(i, j);
+    long long m, k;
+    cin >> n >> m >> k;
+
+    for (long long i = 1; i <= n; i++) dp[i][0] = m;
+    for (long long i = 1; i <= n; i++) {
+        for (long long j = 1; j <= k; j++) {
+            dp[i][j] = (dp[i - 1][j] + (m - 1) * dp[i - 1][j - 1] % MOD) % MOD;
         }
     }
 
-    cin >> n;
-    vector<long long> res;
-    bool flg = true;
-    res.push_back(0);
-    for (long long i = 1; i <= n / 2; i++) {
-        long long u;
-        cin >> u;
-        bool flag = false;
-        for (auto it:buf[u]) {
-            if (it.first > res.back()) {
-                res.push_back(it.first), res.push_back(it.second);
-                flag = true;
-                break;
-            }
-        }
-        if (!flag) {
-            flg = false;
-        }
-    }
-    if (flg) {
-        cout << "Yes" << endl;
-        for (long long i = 1; i < res.size(); i++) {
-            cout << res[i] * res[i] - res[i - 1] * res[i - 1] << " ";
-        }
-        cout << endl;
-    } else {
-        cout << "No" << endl;
-    }
+    cout << dp[n][k] << endl;
 
 
 #ifndef ONLINE_JUDGE
