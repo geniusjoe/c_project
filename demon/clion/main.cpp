30b8b978b88f81c23e4e1b86c0912726ad6bb81f
geniusjoe <843520313@qq.com>
2019-10-15 00:29:23 +0800

    2018-2019 ACM-ICPC, Asia East Continent Finals I

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -13,34 +13,8 @@ const long long OVER_FLOW = 0x7fffffff;
 const long long LOVER_FLOW = 0x7fffffffffffffff;
 
 long long n;
-struct op {
-    long long x1, y1, x2, y2;
-};
-long long vis[MAXN][MAXN];
-vector<op> res;
-string str;
-
-
-void add(long long x1, long long y1, long long x2, long long y2) {
-    res.push_back((op) {x1, y1, x2, y2});
-    vis[x2][y2]++;
-}
-
-void move(long long x1, long long y1, bool x) {
-    if (x) {
-        if (x1 != 1) {
-            add(x1, y1, 1, y1);
-        } else {
-            add(x1, y1, 1, y1 == 1 ? 2 : 1);
-        }
-    } else {
-        if (x1 != 2) {
-            add(x1, y1, 2, y1);
-        } else {
-            add(x1, y1, 2, y1 == 1 ? 2 : 1);
-        }
-    }
-}
+long long dp[2][MAXN][6000];
+long long buf[MAXN][4];
 
 int main() {
 
@@ -72,51 +46,34 @@ int main() {
 */
 
     ios::sync_with_stdio(false);
-    long long m;
-    cin >> n >> m;
-    for (long long i = 1; i <= n; i++) {
-        for (long long j = 1; j <= m; j++) {
-            cin >> str;
-            for (long long k = str.size() - 1; k >= 0; k--) {
-                move(i, j, str[k] == '1');
+    long long T;
+    cin >> T;
+    while (T--) {
+        cin >> n;
+        memset(dp, 0, sizeof dp);
+        for (long long i = 1; i <= n; i++) {
+            cin >> buf[i][1] >> buf[i][2] >> buf[i][3];
+        }
+        dp[n & 1][1][n] = buf[n][1];
+        for (long long i = n - 1; i >= 1; i--) {
+            for (long long j = 1; j <= n - i; j++) {
+                long long down = (2 * i + j) * j / 2, up = (2 * n - j + 1) * j / 2;
+                for (long long k = down; k <= up; k++) {
+                    dp[i & 1][j + 1][k + i] = max(dp[i & 1][j + 1][k + i], dp[(i + 1) & 1][j][k] + buf[i][1]);
+                    dp[i & 1][j][k] = max(dp[1 & i][j][k], dp[(i + 1) & 1][j][k] + buf[i][2] * (k - i * j));
+                    dp[i & 1][j][k] = max(dp[i & 1][j][k], dp[(i + 1) & 1][j][k] + buf[i][3] * j);
+                }
             }
         }
-    }
-    for (long long i = 1; i <= 2; i++) {
-        for (long long j = 2; j <= m; j++) {
-            while (vis[i][j]) {
-                add(i, j, i, 1);
-                vis[i][j]--;
-            }
-        }
-    }
 
-    long long cnt = res.size();
-    memset(vis, 0, sizeof vis);
-
-    for (long long i = 1; i <= n; i++) {
-        for (long long j = 1; j <= m; j++) {
-            cin >> str;
-            for (auto k:str) {
-                move(i, j, k == '1');
+        long long res = 0;
+        for (long long i = 1; i <= n; i++) {
+            for (long long j = 1; j <= 5050; j++) {
+                res = max(res, dp[1][i][j]);
             }
         }
-    }
-    for (long long i = 1; i <= 2; i++) {
-        for (long long j = 2; j <= m; j++) {
-            while (vis[i][j]) {
-                add(i, j, i, 1);
-                vis[i][j]--;
-            }
-        }
-    }
 
-    cout << res.size() << '\n';
-    for (long long i = 0; i < cnt; i++) {
-        cout << res[i].x1 << " " << res[i].y1 << " " << res[i].x2 << " " << res[i].y2 << '\n';
-    }
-    for (long long i = res.size() - 1; i >= cnt; i--) {
-        cout << res[i].x2 << " " << res[i].y2 << " " << res[i].x1 << " " << res[i].y1 << '\n';
+        cout << res << '\n';
     }
 
 
