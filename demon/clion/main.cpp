d5c19a0c5fdabc06ceb6708538735888e62f772a
geniusjoe <843520313@qq.com>
2019-10-18 12:24:40 +0800

    Codeforces Round #510 (Div. 2) E

--- demon/clion/main.cpp
+++ demon/clion/main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const long long MAXN = 200500;
+const long long MAXN = 2005000;
 const long long PHI = (long long) 998244352;
 const long long INF = 0x3f3f3f3f;
 const long long LINF = 0x3f3f3f3f3f3f3f3f;
@@ -13,7 +13,44 @@ const long long OVER_FLOW = 0x7fffffff;
 const long long LOVER_FLOW = 0x7fffffffffffffff;
 
 long long n;
-vector<long long> lft, rgt;
+
+struct point {
+    long long x, y, c, f;
+
+    bool operator<(point point1) {
+        return this->c < point1.c;
+    }
+} points[MAXN];
+
+//******************************
+//返回d=gcd(a,b); 和对应于等式ax+by=d 中的x,y
+long long extend_gcd(long long a, long long b, long long &x, long long &y) {
+    if (a == 0 && b == 0) return -1;//无最大公约数
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    long long d = extend_gcd(b, a % b, y, x);
+    y -= a / b * x;
+    return d;
+}
+
+//********* 求逆元*******************
+//ax = 1(mod n)
+long long inv(long long a, long long n) {
+    long long x, y;
+    long long d = extend_gcd(a, n, x, y);
+    if (d == 1) return (x % n + n) % n;
+    else return -1;
+}
+
+
+void add(long long &x, long long y, long long mod) {  //手动取模
+    x += y;
+    while (x < 0) x += mod;
+    while (x >= mod) x -= mod;
+}
 
 int main() {
 
@@ -45,19 +82,50 @@ int main() {
 */
 
     ios::sync_with_stdio(false);
-    cin >> n;
+    long long m, tarx, tary;
+    cin >> n >> m;
     for (long long i = 1; i <= n; i++) {
-        long long u, v;
-        cin >> u >> v;
-        lft.push_back(u), rgt.push_back(v);
+        for (long long j = 1; j <= m; j++) {
+            long long u;
+            cin >> u;
+            points[(i - 1) * m + j] = {i, j, u};
+        }
     }
-    sort(lft.begin(), lft.end()), sort(rgt.begin(), rgt.end());
-    long long res = n;
-    for (long long i = 0; i < n; i++) {
-        res += max(lft[i], rgt[i]);
-    }
-    cout << res << endl;
+    cin >> tarx >> tary;
 
+    sort(points + 1, points + 1 + n * m);
+
+    long long s, sf, sx, sx2, sy, sy2;
+    s = sf = sx = sx2 = sy = sy2 = 0;
+    points[n * m + 1].c = -1;
+    for (long long i = 1; i <= n * m; i++) {
+        long long fi = 0;
+        if (s) {
+            add(fi, sf, MOD);
+            add(fi, s * points[i].x % MOD * points[i].x % MOD, MOD);
+            add(fi, -2ll * points[i].x % MOD * sx % MOD, MOD);
+            add(fi, sx2, MOD);
+            add(fi, s * points[i].y % MOD * points[i].y % MOD, MOD);
+            add(fi, -2ll * points[i].y % MOD * sy % MOD, MOD);
+            add(fi, sy2, MOD);
+            fi = fi * inv(s, MOD) % MOD;
+            points[i].f = fi;
+        }
+        if (points[i].x == tarx and points[i].y == tary) {
+            cout << fi << endl;
+        }
+        if (points[i].c != points[i + 1].c) {
+            for (long long j = i; j >= 1; j--) {
+                if (points[j].c != points[i].c) break;
+                add(sf, points[j].f, MOD);
+                add(s, 1, MOD);
+                add(sx, points[j].x, MOD);
+                add(sx2, points[j].x * points[j].x % MOD, MOD);
+                add(sy, points[j].y, MOD);
+                add(sy2, points[j].y * points[j].y % MOD, MOD);
+            }
+        }
+    }
 
 #ifndef ONLINE_JUDGE
     auto end_time = clock();
